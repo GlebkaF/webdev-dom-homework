a63ddf6d5cf9068741bcec09c3cc02303941c0ed
Greblips <Evgenspilbegrg@gmail.com>
2023-03-11 19:09:12 +0300

    Complete homework

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const addForm = document.querySelector('input');
 const addComment = document.getElementById("add-button");
 const removeComment = document.getElementById("remove-comment");
@@ -5,6 +6,9 @@ const listElement = document.getElementById("comments");
 const nameInputElement = document.getElementById("name-input");
 const commentInputElement = document.getElementById("comment-input");
 const mainForm = document.querySelector(".add-form");
+const studentElements = document.querySelectorAll(".comment");
+const likeButton = document.querySelectorAll(".like-button");
+
 
 const options = {
   year: '2-digit',
@@ -17,80 +21,186 @@ const options = {
 };
 
 
-let now = new Date().toLocaleString("ru-RU",options).replace(",", "");
+let nowDate = new Date().toLocaleString("ru-RU", options).replace(",", "");
 
 
+
+const comments = [
+  {
+    name: "Глеб Фокин",
+    date: "12.02.22 12:18",
+    comment: "Это будет первый комментарий на этой странице",
+    likesCount: 75,
+    isLiked: false,
+    isEdit: false,
+    
+  },
+  {
+    name: "Варвара Н.",
+    date: "13.02.22 19:22",
+    comment: " Мне нравится как оформлена эта страница! ❤",
+    likesCount: 12,
+    isLiked: true,
+    isEdit: false,
+   
+  },
+
+];
+
+
+
+
+
+
+// Функция удаления последнего комментария
+const delComment = () => {
+  const elem = document.getElementById("comments").lastChild;
+  elem.parentNode.removeChild(elem);
+}
+
+// Отчистка данных с поля
 
 const delValue = () => {
   nameInputElement.value = "";
   commentInputElement.value = "";
 };
 
-addComment.addEventListener("click",  () => {
+
+// Добавление лайка
+
+const addLike =() => {
+  const likeButtons = listElement.querySelectorAll('.like-button');
+  for(let likeButton of likeButtons){
+
+    likeButton.addEventListener('click', () => {
+          const index = likeButton.dataset.index;
+
+          if (comments[index].isLiked === false) {
+            comments[index].isLiked = true;
+            comments[index].likesCount +=1;
+
+          } else if (comments[index].isLiked === true){
+            comments[index].isLiked = false;
+            comments[index].likesCount -=1;
+          }
+
+          renderComments();
+      })
+  }
+}
+
+
+
+// Редактирование  (функцию так и не смог дописать,не хватает знаний,редактирование включается,а сохранить не могу)
+const editText = ( ) => {
+  const editButtons = listElement.querySelectorAll('.edit_comment');
+  for(let editButton of  editButtons){
+
+    editButton.addEventListener('click', () => {
+          const index = editButton.dataset.index;
+
+
+          if (comments[index].isEdit === false) {
+            comments[index].isEdit = true;
+
+          } else if (comments[index].isEdit === true){
+            comments[index].isEdit = false;
+
+
+          }
+
+          renderComments();
+      })
+  }
+}
+
+// Рендер разметки
+
+const renderComments = () => {
+  const userHtml = comments.map((user, index) => {
+    return `<li class="comment" >
+    <div class="comment-header">
+      <div>${user.name}</div>
+      <div>${user.date}</div>
+    </div>
+    <div class="comment-body">
+    ${user.isEdit ? `<textarea class ="aria-text">${user.comment}</textarea>` :`<div class ="comment-text"> ${user.comment} </div>`}
+    </div>
+    <div class="comment-footer">
+      <div class="likes">
+        <span class="likes-counter">${user.likesCount}</span>
+        <button data-index="${index}"  class="${user.isLiked ? 'like-button -active-like' : 'like-button'}"></button>
+        <button data-index="${index}" class= "edit_comment">Редатировать</button>
+       
+      </div>
+    </div>
+  </li>`
+  }).join('')
+
+  listElement.innerHTML = userHtml;
+
+  delValue() // Отчиска формы
+  addLike()  // лайки
+  editText()
+ 
+}
+
+
+
+renderComments();
+
+
+// Добавление комментария
+addComment.addEventListener("click", () => {
 
   if (nameInputElement.value === "" || commentInputElement.value === "") {
     nameInputElement.classList.add("error");
     commentInputElement.classList.add("error");
     nameInputElement.placeholder = 'Введите имя';
     commentInputElement.placeholder = 'Введите комментарий';
-    nameInputElement.value = '';
-    commentInputElement.value = '';
     buttonBlock()
     return;
 
-  }else{
-    
+  } else {
+    comments.push({
+      name: nameInputElement.value,
+      date: nowDate,
+      comment: commentInputElement.value,
+      likesCount: 0,
+      isLiked: false,
+      isEdit: false,
+    })
+   
+  
+
     nameInputElement.classList.remove("error");
     commentInputElement.classList.remove("error");
     const oldListHtml = listElement.innerHTML;
-    listElement.innerHTML =
-      oldListHtml +
-      `<li class="comment" >
-      <div class="comment-header">
-        <div>${nameInputElement.value}</div>
-        <div>${now}</div>
-      </div>
-      <div class="comment-body">
-        <div class="comment-text">
-          ${commentInputElement.value}
-        </div>
-      </div>
-      <div class="comment-footer">
-        <div class="likes">
-          <span class="likes-counter">0</span>
-          <button class="like-button" id ="like-input"></button>
-        </div>
-      </div>
-    </li>`;
+
 
   }
+  renderComments();
   
-  delValue();
 
 });
 
 
 
-// Функция удаления последнего комментария
-  const  delComment = () => {
-  const elem = document.getElementById("comments").lastChild;
-  elem.parentNode.removeChild(elem);
-}
 
-removeComment.addEventListener("click",  () => {
- delComment()
+removeComment.addEventListener("click", () => {
+  delComment()
 })
 
 // Блокировка кнопки ввода()
-const buttonBlock = () =>{
-document.querySelectorAll("#name-input,#comment-input").forEach((el)=>{
- 		el.addEventListener("input",()=>{
-       if (document.getElementById("name-input").value === '' || document.getElementById("comment-input").value === '')
-          document.getElementById("add-button").disabled = true;
+const buttonBlock = () => {
+  document.querySelectorAll("#name-input,#comment-input").forEach((el) => {
+    el.addEventListener("input", () => {
+      if (document.getElementById("name-input").value === '' || document.getElementById("comment-input").value === '')
+        document.getElementById("add-button").disabled = true;
       else
-        document.getElementById("add-button").disabled = false; 
+        document.getElementById("add-button").disabled = false;
     });
- });
+  });
 }
 
 
@@ -101,6 +211,9 @@ mainForm.addEventListener('keyup', (e) => {
     delValue();
   }
 });
+
+
+
 
 
 
