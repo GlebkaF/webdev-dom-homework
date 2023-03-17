cbaa714d1a3059bb7e56d4ced07ea0db30bb2123
Nikita Mishin <nikita.mishin.91@list.ru>
2023-03-17 19:17:57 +0300

    HW 2.11

--- script.js
+++ script.js
@@ -1,62 +1,186 @@
-const commentsElement = document.getElementById('commentUl');
-const nameInputElement = document.querySelector('.add-form-name');
-const textInputElement = document.querySelector('.add-form-text');
-const buttonElement = document.querySelector('.add-form-button');
-const buttonDelete = document.querySelector('.deleteButton');
-const commentObjectUl = [
+const listElement = document.getElementById("list");
+const quoteElement = document.getElementById("form__quote");
+const quoteNameElement = document.getElementById("quote__name");
+const quoteBodyElement = document.getElementById("quote__body");
+const inputNameElement = document.getElementById("input__name");
+const buttonQuoteElement = document.getElementById("button__quote");
+const buttonWriteElement = document.getElementById("button__write");
+const buttonDeleteElement = document.getElementById("button__delete");
+const textareaCommentElement = document.getElementById("textarea__comment");
+const listObject = [
     {
         name: "Глеб Фокин",
         date: "12.02.22 12:18",
         comment: "Это будет первый комментарий на этой странице",
         like: 3,
-        hard: ""
+        hard: "",
+        textRedact: true,
+        quoteName: "",
+        quoteComment: ""
     },
     {
         name: "Варвара Н.",
         date: "13.02.22 19:22",
         comment: "Мне нравится как оформлена эта страница! ❤",
         like: 75,
-        hard: "-active-like"
+        hard: "-active-like",
+        textRedact: true,
+        quoteName: "",
+        quoteComment: ""
     }
 ];
 
 renderComment();
 
-buttonElement.disabled = true;
-buttonElement.addEventListener('click', newComment);
-nameInputElement.addEventListener('keyup', checkEnter);
-textInputElement.addEventListener('keyup', checkEnter);
-buttonDelete.addEventListener('click', deleteLi);
+buttonWriteElement.disabled = true;
+buttonWriteElement.addEventListener('click', newComment);
+inputNameElement.addEventListener('keyup', checkEnter);
+textareaCommentElement.addEventListener('keyup', checkEnter);
+buttonDeleteElement.addEventListener('click', deleteComment);
 
 window.addEventListener('input', function () {
-    if (nameInputElement.value === "" || textInputElement.value === "") {
-        buttonElement.disabled = true;
+    if (inputNameElement.value === "" || textareaCommentElement.value === "") {
+        buttonWriteElement.disabled = true;
     } else {
-        buttonElement.disabled = false;
+        buttonWriteElement.disabled = false;
     }
 }, false);
 
 function newComment() {
-    nameInputElement.classList.remove("error")
-    textInputElement.classList.remove("error")
-    if (nameInputElement.value === "" || textInputElement.value === "") {
-        if (nameInputElement.value === "") nameInputElement.classList.add("error")
-        if (textInputElement.value === "") textInputElement.classList.add("error")
+    inputNameElement.classList.remove("-background-error")
+    textareaCommentElement.classList.remove("-background-error")
+    if (inputNameElement.value === "" || textareaCommentElement.value === "") {
+        if (inputNameElement.value === "") inputNameElement.classList.add("-background-error")
+        if (textareaCommentElement.value === "") textareaCommentElement.classList.add("-background-error")
         return;
     }
 
-    commentObjectUl.push({
-        name: nameInputElement.value,
+    listObject.push({
+        name: safeHtmlString(inputNameElement.value),
         date: date(),
-        comment: textInputElement.value,
+        comment: safeHtmlString(textareaCommentElement.value),
         like: 0,
-        hard: ""
+        hard: "",
+        textRedact: true,
+        quoteName: quoteNameElement.innerHTML,
+        quoteComment: quoteBodyElement.innerHTML
     });
 
     renderComment();
 
-    nameInputElement.value = "";
-    textInputElement.value = "";
+    inputNameElement.value = "";
+    textareaCommentElement.value = "";
+    quoteNameElement.innerHTML = "";
+    quoteBodyElement.innerHTML = "";
+    quoteElement.classList.add("-display-none")
+}
+
+function renderComment() {
+    listElement.innerHTML = listObject.map((listObject, i) => {
+        let result = [];
+        if (!listObject.quoteName) {
+            result = `
+            <li class="comments__list">
+                <section class="comments__header">
+                    <p>${listObject.name}</p>
+                    <p>${listObject.date}</p>
+                </section>
+                <div id="comments__quote" class="comments__quote -display-none">
+                    <p>${listObject.quoteName}</p>
+                    <p>${listObject.quoteComment}</p>
+                </div>
+                <section data-comments__body="${i}" class="comments__body">
+                    <p>${listObject.comment}</p>
+                </section>
+                <div class="comments__footer">
+                    <button data-button__redact="${i}" class="comments__button-redact">Редактировать</button>
+                    <div>
+                        <span>${listObject.like}</span>
+                        <button data-button__hard="${i}" class="comments__button-hard ${listObject.hard}"></button>
+                    </div>
+                </div>
+            </li>`
+        } else {
+            result = `
+            <li class="comments__list">
+                <section class="comments__header">
+                    <p>${listObject.name}</p>
+                    <p>${listObject.date}</p>
+                </section>
+                <div id="comments__quote" class="comments__quote">
+                    <p>${listObject.quoteName}:</p>
+                    <p>${listObject.quoteComment}</p>
+                </div>
+                <section data-comments__body="${i}" class="comments__body">
+                    <p>${listObject.comment}</p>
+                </section>
+                <div class="comments__footer">
+                    <button data-button__redact="${i}" class="comments__button-redact">Редактировать</button>
+                    <div>
+                        <span>${listObject.like}</span>
+                        <button data-button__hard="${i}" class="comments__button-hard ${listObject.hard}"></button>
+                    </div>
+                </div>
+            </li>`
+        }
+        return result;
+    }).join("");
+
+    const buttonHardElements = document.querySelectorAll(".comments__button-hard");
+
+    for (const buttonHardElement of buttonHardElements) {
+        buttonHardElement.addEventListener("click", () => {
+            const i = buttonHardElement.dataset.button__hard;
+            if (!listObject[i].hard) {
+                listObject[i].hard = "-active-like"
+                listObject[i].like += 1;
+                renderComment();
+            } else {
+                listObject[i].hard = ""
+                listObject[i].like -= 1;
+                renderComment();
+            }
+        });
+    }
+
+    const buttonRedactElements = document.querySelectorAll(".comments__button-redact");
+
+    for (const buttonRedactElement of buttonRedactElements) {
+        buttonRedactElement.addEventListener("click", () => {
+            const i = buttonRedactElement.dataset.button__redact;
+            if (buttonRedactElements[i].innerHTML === "Редактировать") {
+                buttonRedactElements[i].innerHTML = "Сохранить";
+                listObject[i].textRedact = false;
+                const commentBodyElements = document.querySelectorAll(".comments__body");
+                const commentBodyElement = commentBodyElements[i];
+                const textareaElement = `<textarea type="textarea" class="-redactor-textarea" rows="4">${listObject[i].comment}</textarea>`;
+                commentBodyElement.innerHTML = textareaElement;
+            } else {
+                listObject[i].textRedact = true;
+                const redactCommentElement = document.querySelectorAll(".-redactor-textarea");
+                listObject[i].comment = safeHtmlString(redactCommentElement[0].value);
+                renderComment();
+            }
+        });
+    }
+
+    const commentQuoteElements = document.querySelectorAll(".comments__body");
+
+    for (const commentQuoteElement of commentQuoteElements) {
+        commentQuoteElement.addEventListener("click", () => {
+            const i = commentQuoteElement.dataset.comments__body;
+            if (!listObject[i].textRedact) return;
+            quoteNameElement.innerHTML = listObject[i].name;
+            quoteBodyElement.innerHTML = listObject[i].comment;
+            quoteElement.classList.remove("-display-none")
+        });
+    }
+
+    buttonQuoteElement.addEventListener("click", () => {
+        quoteElement.classList.add("-display-none")
+        quoteNameElement.innerHTML = "";
+        quoteBodyElement.innerHTML = "";
+    });
 }
 
 function date() {
@@ -79,69 +203,16 @@ function checkEnter(key) {
     }
 }
 
-function deleteLi() {
-    commentObjectUl.pop();
+function deleteComment() {
+    listObject.pop();
     renderComment();
 }
 
-function renderComment() {
-    const commentHtml = commentObjectUl.map((commentObjectUl, i) => {
-        return `
-        <li class="comment">
-            <div class="comment-header">
-                <div>${commentObjectUl.name}</div>
-                <div>${commentObjectUl.date}</div>
-            </div>
-            <div id="check" class="comment-body">
-                <div id="redactor${i}" class="comment-text">
-                    ${commentObjectUl.comment}
-                </div>
-            </div>
-            <button data-redactor="${i}" class="redactorButton">Редактировать</button>
-            <div class="comment-footer">
-                <div class="likes">
-                    <span class="likes-counter">${commentObjectUl.like}</span>
-                    <button data-like="${i}" class="like-button ${commentObjectUl.hard}"></button>
-                </div>
-            </div>
-        </li>
-                `
-    }).join("");
-
-    commentsElement.innerHTML = commentHtml;
-
-    const likeButtonElements = document.querySelectorAll(".like-button");
-
-    for (const likeButtonElement of likeButtonElements) {
-        likeButtonElement.addEventListener("click", () => {
-            const i = likeButtonElement.dataset.like;
-            if (commentObjectUl[i].hard === "") {
-                commentObjectUl[i].hard = "-active-like"
-                commentObjectUl[i].like += 1;
-                renderComment();
-            } else {
-                commentObjectUl[i].hard = ""
-                commentObjectUl[i].like -= 1;
-                renderComment();
-            }
-        });
-    }
-
-    const redactorButtonElements = document.querySelectorAll(".redactorButton");
-    for (const redactorButtonElement of redactorButtonElements) {
-        redactorButtonElement.addEventListener("click", () => {
-            const i = redactorButtonElement.dataset.redactor;
-            if (redactorButtonElements[i].innerHTML === "Редактировать") {
-                const redact = `redactor${i}`;
-                const commentOld = document.getElementById(redact);
-                const textarea = `<textarea id="check" type="textarea" class="redactorTextArea" rows="4">${commentObjectUl[i].comment}</textarea>`;
-                redactorButtonElements[i].innerHTML = "Сохранить";
-                commentOld.innerHTML = textarea;
-            } else {
-                const redactCommentElement = document.querySelectorAll(".redactorTextArea");
-                commentObjectUl[i].comment = redactCommentElement[0].value;
-                renderComment();
-            }
-        });
-    }
+function safeHtmlString(str) {
+    str = str
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;");
+    return str;
 }
