bbdec0ffa9240e6bea87dade5c579891d5362086
Ilia Abramov <akoostakai@gmail.com>
2023-03-21 20:23:36 +0300

    hw-2.12

--- script.js
+++ script.js
@@ -7,28 +7,8 @@ const buttonQuoteElement = document.getElementById("button__quote");
 const buttonWriteElement = document.getElementById("button__write");
 const buttonDeleteElement = document.getElementById("button__delete");
 const textareaCommentElement = document.getElementById("textarea__comment");
-const listObject = [
-    {
-        name: "Глеб Фокин",
-        date: "12.02.22 12:18",
-        comment: "Это будет первый комментарий на этой странице",
-        like: 3,
-        hard: "",
-        textRedact: true,
-        quoteName: "",
-        quoteComment: ""
-    },
-    {
-        name: "Варвара Н.",
-        date: "13.02.22 19:22",
-        comment: "Мне нравится как оформлена эта страница! ❤",
-        like: 75,
-        hard: "-active-like",
-        textRedact: true,
-        quoteName: "",
-        quoteComment: ""
-    }
-];
+let loader = "no";
+let listObject = [];
 
 renderComment();
 
@@ -55,104 +35,122 @@ function newComment() {
         return;
     }
 
-    listObject.push({
-        name: safeHtmlString(inputNameElement.value),
-        date: date(),
-        comment: safeHtmlString(textareaCommentElement.value),
-        like: 0,
-        hard: "",
-        textRedact: true,
-        quoteName: quoteNameElement.innerHTML,
-        quoteComment: quoteBodyElement.innerHTML
+    loader = "yes";
+    fetch("https://webdev-hw-api.vercel.app/api/v1/ilia-abramov/comments", {
+        method: "POST",
+        body: JSON.stringify({
+            text: textareaCommentElement.value,
+            name: inputNameElement.value
+        }),
+    }).then(() => {
+        renderComment();
     });
-
-    renderComment();
 
     inputNameElement.value = "";
     textareaCommentElement.value = "";
     quoteNameElement.innerHTML = "";
     quoteBodyElement.innerHTML = "";
-    quoteElement.classList.add("-display-none")
+    quoteElement.classList.add("-display-none");
 }
 
 function renderComment() {
-    listElement.innerHTML = listObject.map((listObject, i) => `
-    <li class="comments__list">
-        <section class="comments__header">
-            <p>${listObject.name}</p>
-            <p>${listObject.date}</p>
-        </section>
-        <div id="comments__quote" class="comments__quote ${!listObject.quoteName ? '-display-none' : ''}">
-            <p>${listObject.quoteName}</p>
-            <p>${listObject.quoteComment}</p>
-        </div>
-        <section data-comments="${i}" class="comments__body">
-            <p>${listObject.comment}</p>
-        </section>
-        <div class="comments__footer">
-            <button data-button__redact="${i}" class="comments__button-redact">Редактировать</button>
-            <div>
-                <span>${listObject.like}</span>
-                <button data-button__hard="${i}" class="comments__button-hard ${listObject.hard}"></button>
-            </div>
-        </div>
-    </li>`
-    ).join("");
+    const formElements = document.querySelectorAll(".form");
+    const loaderElements = document.querySelectorAll(".loader")
+    if (loader === "yes") {
+        formElements[0].classList.add("-display-none");
+        loaderElements[0].classList.remove("-display-none")
+    }
+    fetch("https://webdev-hw-api.vercel.app/api/v1/ilia-abramov/comments", {
+        method: "GET"
+    }).then((response) => {
+        response.json().then((responseDate) => {
+            listObject = responseDate.comments;
+            listElement.innerHTML = listObject.map((listObject, i) => `
+            <li class="comments__list">
+                <section class="comments__header">
+                    <p>${listObject.author.name}</p>
+                    <p>${correctDate(listObject.date)}</p>
+                </section>
+                <div id="comments__quote" class="comments__quote ${!listObject.quoteName ? '-display-none' : ''}">
+                    <p>${listObject.quoteName}</p>
+                    <p>${listObject.quoteComment}</p>
+                </div>
+                <section data-comments="${i}" class="comments__body">
+                    <p>${listObject.text}</p>
+                </section>
+                <div class="comments__footer">
+                    <button data-redact="${i}" class="comments__button-redact">Редактировать</button>
+                    <div>
+                        <span class="likes">${listObject.likes}</span>
+                        <button data-hard="${i}" class="comments__button-hard"></button>
+                    </div>
+                </div>
+            </li>`
+            ).join("");
+            
+            formElements[0].classList.remove("-display-none");
+            loaderElements[0].classList.add("-display-none")
 
-    const buttonHardElements = document.querySelectorAll(".comments__button-hard");
+            const buttonHardElements = document.querySelectorAll(".comments__button-hard");
 
-    for (const buttonHardElement of buttonHardElements) {
-        buttonHardElement.addEventListener("click", () => {
-            const i = buttonHardElement.dataset.button__hard;
-            if (!listObject[i].hard) {
-                listObject[i].hard = "-active-like"
-                listObject[i].like += 1;
-                renderComment();
-            } else {
-                listObject[i].hard = ""
-                listObject[i].like -= 1;
-                renderComment();
+            for (const buttonHardElement of buttonHardElements) {
+                buttonHardElement.addEventListener("click", () => {
+                    const i = buttonHardElement.dataset.hard;
+                    const likes = document.querySelectorAll(".likes");
+                    const buttonLikes = document.querySelectorAll(".comments__button-hard");
+                    if (!listObject[i].isLiked) {
+                        listObject[i].isLiked = true;
+                        listObject[i].likes += 1;
+                        likes[i].innerHTML = listObject[i].likes;
+                        buttonLikes[i].classList.add("-active-like");
+                    } else {
+                        listObject[i].isLiked = false;
+                        listObject[i].likes -= 1;
+                        likes[i].innerHTML = listObject[i].likes;
+                        buttonLikes[i].classList.remove("-active-like");
+                    }
+                });
             }
-        });
-    }
 
-    const buttonRedactElements = document.querySelectorAll(".comments__button-redact");
+            const buttonRedactElements = document.querySelectorAll(".comments__button-redact");
 
-    for (const buttonRedactElement of buttonRedactElements) {
-        buttonRedactElement.addEventListener("click", () => {
-            const i = buttonRedactElement.dataset.button__redact;
-            if (buttonRedactElements[i].innerHTML === "Редактировать") {
-                buttonRedactElements[i].innerHTML = "Сохранить";
-                listObject[i].textRedact = false;
-                const commentBodyElements = document.querySelectorAll(".comments__body");
-                const commentBodyElement = commentBodyElements[i];
-                const textareaElement = `<textarea type="textarea" class="-redactor-textarea" rows="4">${listObject[i].comment}</textarea>`;
-                commentBodyElement.innerHTML = textareaElement;
-            } else {
-                listObject[i].textRedact = true;
-                const redactCommentElement = document.querySelectorAll(".-redactor-textarea");
-                listObject[i].comment = safeHtmlString(redactCommentElement[0].value);
-                renderComment();
+            for (const buttonRedactElement of buttonRedactElements) {
+                buttonRedactElement.addEventListener("click", () => {
+                    const i = buttonRedactElement.dataset.redact;
+                    if (buttonRedactElements[i].innerHTML === "Редактировать") {
+                        buttonRedactElements[i].innerHTML = "Сохранить";
+                        listObject[i].textRedact = false;
+                        const commentBodyElements = document.querySelectorAll(".comments__body");
+                        const commentBodyElement = commentBodyElements[i];
+                        const textareaElement = `<textarea type="textarea" class="-redactor-textarea" rows="4">${listObject[i].comment}</textarea>`;
+                        commentBodyElement.innerHTML = textareaElement;
+                    } else {
+                        listObject[i].textRedact = true;
+                        const redactCommentElement = document.querySelectorAll(".-redactor-textarea");
+                        listObject[i].comment = safeHtmlString(redactCommentElement[0].value);
+                        renderComment();
+                    }
+                });
             }
+
+            const commentQuoteElements = document.querySelectorAll(".comments__body");
+
+            for (const commentQuoteElement of commentQuoteElements) {
+                commentQuoteElement.addEventListener("click", () => {
+                    const i = commentQuoteElement.dataset.comments;
+                    if (!listObject[i].textRedact) return;
+                    quoteNameElement.innerHTML = listObject[i].name;
+                    quoteBodyElement.innerHTML = listObject[i].comment;
+                    quoteElement.classList.remove("-display-none")
+                });
+            }
+
+            buttonQuoteElement.addEventListener("click", () => {
+                quoteElement.classList.add("-display-none")
+                quoteNameElement.innerHTML = "";
+                quoteBodyElement.innerHTML = "";
+            });
         });
-    }
-
-    const commentQuoteElements = document.querySelectorAll(".comments__body");
-
-    for (const commentQuoteElement of commentQuoteElements) {
-        commentQuoteElement.addEventListener("click", () => {
-            const i = commentQuoteElement.dataset.comments;
-            if (!listObject[i].textRedact) return;
-            quoteNameElement.innerHTML = listObject[i].name;
-            quoteBodyElement.innerHTML = listObject[i].comment;
-            quoteElement.classList.remove("-display-none")
-        });
-    }
-
-    buttonQuoteElement.addEventListener("click", () => {
-        quoteElement.classList.add("-display-none")
-        quoteNameElement.innerHTML = "";
-        quoteBodyElement.innerHTML = "";
     });
 }
 
@@ -170,6 +168,12 @@ function date() {
     return day + "." + month + "." + year + " " + hour + ":" + minute;
 }
 
+function correctDate(str) {
+    const time = str.split("");
+    const result = `${time[8] + time[9] + "." + time[5] + time[6] + "." + time[0] + time[1] + time[2] + time[3] + " " + time[11] + time[12] + time[13] + time[14] + time[15]}`;
+    return result;
+}
+
 function checkEnter(key) {
     if (key.code === "Enter" || key.code === "NumpadEnter") {
         newComment();
@@ -177,15 +181,14 @@ function checkEnter(key) {
 }
 
 function deleteComment() {
-    listObject.pop();
     renderComment();
 }
 
 function safeHtmlString(str) {
     str = str
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;");
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;");
     return str;
 }
