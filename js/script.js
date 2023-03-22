97a7d3b2cd6b05c5286356b6041fb6c69f9a585a
Alex Volo <alex_volo@bk.ru>
2023-03-22 09:27:14 +0900

    All tasks done

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
 'use strict';
 
-// Достаем комментарии с сервера
-function getComments(){
-fetch('https://webdev-hw-api.vercel.app/api/v1/alex-volo/comments', {
-    method: "GET"
-}).then((response) => 
-response.json().then((responseData) => {
-    comments = responseData.comments;
-    renderComments()
-}));
+
+// За неимением кнута его долго били пряником...
+
+
+// Заглушка при загрузке комментариев с сервера
+const commentsList = document.querySelector('ul.comments');
+let comments = [{
+    date: getDate(new Date),
+    isLiked: false,
+    likes: 1000,
+    text: "Комментарии загружаются........",
+    author: { name: '' }
+}
+];
+renderComments();
+
+// Достаю комментарии с сервера
+function getComments() {
+    fetch('https://webdev-hw-api.vercel.app/api/v1/alex-volo/comments', {
+        method: "GET"
+    }).then((response) =>
+        response.json().then((responseData) => {
+            comments = responseData.comments;
+            renderComments()
+        }));
 }
 getComments();
 
-// Форма и её инпуты
+// Форма
 const addForm = document.querySelector('div.add-form');
-const inputName = document.querySelector('input.add-form-name');
-const inputComment = document.querySelector('div.add-form > textarea.add-form-text');
-const buttonAddComment = document.querySelector('button.add-form-button');
-// UL 
-const commentsList = document.querySelector('ul.comments');
 
 // Массив с данными о комментариях
+// Пока оставляю, чтобы не лезли ошибки
 const commentsListArray = [];
-let comments = [];
 
-// Функция преобразует дату из строки
+// Функция преобразует дату из строки используется в рендер функции
 // Работает, но мне не нравится как написана 
 function getDate(date) {
     const options = {
@@ -40,20 +51,19 @@ function getDate(date) {
 
 // Функция рисует HTML-разметку всех комментариев
 function renderComments() {
-    commentsList.innerHTML = comments.reduce((result, comment) => {
+
+    commentsList.innerHTML = comments.reduce((result, comment, index) => {
         return result + `
-    <li class="comment" data-id="${comment.id}">
+    <li class="comment" data-id="${comment.id}" data-index="${index}">
         <div class="comment-header">
-        <div>${comment.author.name}</div>
-        <div>
-            ${getDate(comment.date)}
+        <div>${comment.author.name}
+        </div>
+        <div>${getDate(comment.date)}
         </div >
         </div >
         <div class="comment-body">
-        <div class="comment-text">
-            
-            ${comment.text}
-            
+        <div class="comment-text">   
+            ${comment.text}            
         </div>
         </div>
         <div class="comment-footer">
@@ -61,7 +71,7 @@ function renderComments() {
         <button class="edit-button">Редактировать </button>
         <div class="likes">
             <span class="likes-counter">${comment.likes}</span>
-            <button data-index="${comment.id}"  class="${comment.isLiked ? 'like-button -active-like' : 'like-button'}"></button>
+            <button class="${comment.isLiked ? 'like-button -active-like' : 'like-button'}"></button>
         </div>
         </div>
     </li >`
@@ -70,15 +80,41 @@ function renderComments() {
     addListenerOnComments();
 }
 
+// Рендер формы добавления комментария
+let isLoading = false; //Загружается ли комментарий на сервер
 
-// Попробуем делегировать события и повешать один обработчик
-// На комментарий
+function renderForm() {
+    if (!isLoading) {
+        addForm.innerHTML = `    
+    <input type="text" class="add-form-name" placeholder="Введите ваше имя" id="input-name" />
+    <textarea type="textarea" class="add-form-text" placeholder="Введите ваш коментарий" rows="4"
+      id="input-comment"></textarea>
+    <div class="add-form-row">
+      <button class="add-form-button" id="button-add-comment">Написать</button>
+    </div>`;
+        // Добавляю событие на клик по добавить комментарий
+        const buttonAddComment = document.querySelector('button.add-form-button');
+        buttonAddComment.addEventListener('click', addComment);
+    } else {
+        addForm.innerHTML = `    
+        Комментарий добавляется...
+        <svg class="spinner" viewBox="0 0 50 50">
+        <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+      </svg>
+        `
+    }
+
+
+}
+renderForm();
+
+// Делегирую события для комментариев на один обработчик
 function addListenerOnComments() {
     const currentComments = document.querySelectorAll('li.comment');
 
     for (const comment of currentComments) {
         comment.addEventListener('click', (e) => {
-            const index = comment.dataset.id;
+            const index = comment.dataset.index;
             const likeButton = e.currentTarget.querySelector('button.like-button');
             const editButton = e.currentTarget.querySelector('.edit-button');
             const deleteButton = e.currentTarget.querySelector('.delete-button');
@@ -157,13 +193,13 @@ addForm.addEventListener('keyup', (e) => {
     if (e.code == 'Enter') addComment();
 });
 
-// Событие на кнопку добавить комментарий
-buttonAddComment.addEventListener('click', addComment);
+
 
 // Функция добавляет комментарий
 function addComment() {
+    const inputName = document.querySelector('input.add-form-name');
+    const inputComment = document.querySelector('div.add-form > textarea.add-form-text');
     const currentDate = new Date;
-    const currentDateString = getDate(currentDate)
 
     // Таймаут красного фона на полях
     function clearInputName() {
@@ -190,24 +226,31 @@ function addComment() {
         setTimeout(clearInputComment, 1500);
 
     } else {
+        // Заглушка на время отправки коммента на сервер
+        isLoading = true;
+        renderForm();
+
         fetch('https://webdev-hw-api.vercel.app/api/v1/alex-volo/comments', {
             method: "POST",
+
             body: JSON.stringify({
                 date: currentDate,
                 likes: 0,
                 isLiked: false,
                 text: safeInput(inputComment.value),
                 name: safeInput(inputName.value),
-        })
+            })
+
         }).then((response) => {
             getComments();
-        response.json().then((responseData) => {
-            console.log(responseData);
-        })
-    });
+
+            response.json().then((responseData) => {
+                console.log(responseData);
+                isLoading = false;
+                renderForm();
+            })
+        });
         inputName.value = '';
         inputComment.value = '';
     }
 }
-
-console.log("It works! К моему большому удивлению!");
