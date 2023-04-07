6b8d866cc58d56bf4cc0a16a5bb7a877cf6ccd4a
Greblips <Evgenspilbegrg@gmail.com>
2023-04-07 11:33:02 +0300

    Отдельный модуль входа

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 import { getCommentsList, publicComment } from "./api.js";
 import {getDate,safeInputText,delay} from "./secondaryFunc.js"
-
+import { renderLoginComponent } from "./login-component.js";
 
  let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-let comments = [];
+ let comments = [];
 
-// token = null;
-const waitCommentMessage = document.getElementById("waitComment");
-/* waitCommentMessage.style.display = "inline"; */
+token = null;
+
+// const waitCommentMessage = document.getElementById("waitComment");
+// /* waitCommentMessage.style.display = "inline"; */
 
 
 
@@ -31,74 +32,83 @@ const fetchGetAndRender = () => {
 
 //рендер-функция
 
-const renderComments = () => {
+ export const renderComments = () => {
   const appEl = document.getElementById("app");
 
-  if (!token) {
-    const commentsHtml =
-    comments.map((user, index) => {
-      return `<li class="comment"  data-name="${user.author.name}" data-comment="${user.text}">
-      <div class="comment-header">
-        <div>${user.author.name}</div>
-        <div>${getDate(user.date)}</div>
-      </div>
-      <div class="comment-body" >
-     <div class ="comment-text"> ${user.text} </div>
-      </div>
-      <div class="comment-footer">
-        <div class="likes">
-          <span class="likes-counter">${user.likes}</span>
-          <button data-index="${index}"  class="${user.isLiked ? 'like-button -active-like' : 'like-button'}"></button>
+  
+    if (!token) {
+      renderLoginComponent({
+        comments,
+        appEl,
+        setToken: (newToken) => {
+          token = newToken;
+        },
+        renderComments,
+      });
+      return;
+    }
+    // const commentsHtml =
+    // comments.map((user, index) => {
+    //   return `<li class="comment"  data-name="${user.author.name}" data-comment="${user.text}">
+    //   <div class="comment-header">
+    //     <div>${user.author.name}</div>
+    //     <div>${getDate(user.date)}</div>
+    //   </div>
+    //   <div class="comment-body" >
+    //  <div class ="comment-text"> ${user.text} </div>
+    //   </div>
+    //   <div class="comment-footer">
+    //     <div class="likes">
+    //       <span class="likes-counter">${user.likes}</span>
+    //       <button data-index="${index}"  class="${user.isLiked ? 'like-button -active-like' : 'like-button'}"></button>
       
-        </div>
-      </div>
-    </li>`
-    }).join('')
+    //     </div>
+    //   </div>
+    // </li>`
+    // }).join('')
 
-    const appHtml = `
-            <div class="container">
-              <p id = 'waitComment' style="display: none">Комментарии загружаются...</p>
-              <ul id="list" class="comments">
-                ${commentsHtml}
-              </ul>
-              <p id = 'addingComment' style="display: none">Комментарий добавляется...</p>
+    // const appHtml = `
+    //         <div class="container">
+    //           <p id = 'waitComment' style="display: none">Комментарии загружаются...</p>
+    //           <ul id="list" class="comments">
+    //             ${commentsHtml}
+    //           </ul>
+    //           <p id = 'addingComment' style="display: none">Комментарий добавляется...</p>
               
 
-            <div class="add-form">
-              <h2 class="class">Форма входа</h2>
-              <input
-                type="text"
-                id="login-input"
-                class="add-form-name"
-                placeholder="Введите ваш логин"
-              /> <br/>
-              <input
-                type="text"
-                id="password-input"
-                class="add-form-name"
-                placeholder="Введите ваш пароль"
-              />
+    //         <div class="add-form">
+    //           <h2 class="class">Форма входа</h2>
+    //           <input
+    //             type="text"
+    //             id="login-input"
+    //             class="add-form-name"
+    //             placeholder="Введите ваш логин"
+    //           /> <br/>
+    //           <input
+    //             type="text"
+    //             id="password-input"
+    //             class="add-form-name"
+    //             placeholder="Введите ваш пароль"
+    //           />
               
-              <div class="add-form-row">
-                <button id="login-button" class="add-form-button">Войти</button>
-                <button data-id = 'id' id="reg-button" class="add-form-button">
-                  Зарегистрироваться
-                </button>
-              </div>
-            </div>
+    //           <div class="add-form-row">
+    //             <button id="login-button" class="add-form-button">Войти</button>
+    //             <button data-id = 'id' id="reg-button" class="add-form-button">
+    //               Зарегистрироваться
+    //             </button>
+    //           </div>
+    //         </div>
 
-          </div>`;
+    //       </div>`;
 
-    appEl.innerHTML = appHtml;
+    // appEl.innerHTML = appHtml;
 
-    document.getElementById("login-button").addEventListener("click", () => {
-      token =
-        "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-      renderComments();
-    });
+    // document.getElementById("login-button").addEventListener("click", () => {
+    //   token =
+    //     "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+    //     fetchGetAndRender()
+    // });
 
-    return;
-  }
 
   const commentsHtml =
   comments.map((user, index) => {
