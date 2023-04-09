6c0bfc2794d16026a81b4f3a20abdea7098f4d96
Nataiia Vodopianova <natalvod@yandex.ru>
2023-04-09 20:19:02 +0300

    создает модуль API

---
+++ api.js
@@ -0,0 +1,44 @@
+let host = 'https://webdev-hw-api.vercel.app/api/v2/:natalvod/comments';
+
+export function getComments({ token }) {
+    return fetch(host, {
+        method: "GET",
+        headers: {
+            Authorization: token,
+        }
+    }).then((response) => {
+        if (response.status === 200) {
+            return response.json();
+        } else if (response.status === 401) {
+            throw new Error("Нет авторизации");
+        } else {
+            throw new Error("Сервер сломался, попробуй позже")
+        }
+    });
+};
+
+export function addComment({ name, text, date, forceError, token }) {
+    return fetch(host, {
+        method: "POST",
+        body: JSON.stringify({
+            name,
+            date,
+            text,
+            forceError,
+        }),
+        headers: {
+            Authorization: token,
+        }
+    }).then((response) => {
+
+        if (response.status === 201) {
+            return response.json();
+        } else if (response.status === 500) {
+            alert('Сервер не работает, повторите попытку позже')
+            throw new Error("Сервер сломался, попробуй позже")
+        } else if (response.status === 400) {
+            alert('Имя и комментарий должны быть не короче 3 символов')
+            throw new Error("Имя и комментарий короче 3 символов")
+        }
+    });
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { 
+import { getComments, addComment } from "./api.js";
+import {
   //deleteLastComment, deleteComment 
 } from "./formComments.js";
-import { myDate, secureInput} from "./optionalFunction.js";
+import { myDate, secureInput } from "./optionalFunction.js";
 
 // export const buttonElement = document.querySelector('button.add-form-button');
 export const listElement = document.querySelector('.comments');
@@ -16,27 +17,14 @@ export let comments = [];
 
 //функция GET
 
-let host = 'https://webdev-hw-api.vercel.app/api/v2/:natalvod/comments';
+
 
 let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
 token = null;
 
 export const getFetchPromise = () => {
   //loaderCommentsElement.classList.remove('-display-none');
-  return fetch(host,{
-    method: "GET",
-    headers: {
-      Authorization: token,
-  }
-  }).then((response) => {
-    if (response.status === 200){
-      return response.json();
-     } else if (response.status === 401) {
-        throw new Error("Нет авторизации");
-    } else {
-     throw new Error("Сервер сломался, попробуй позже")
-    };
-  }).then((responseData) => {
+  return getComments({ token }).then((responseData) => {
     const appComments = responseData.comments.map((comment) => {
       return {
         name: secureInput(comment.author.name),
@@ -55,7 +43,19 @@ export const getFetchPromise = () => {
   });
 };
 
-
+export const postFetchPromise = () => {
+  addComment({
+    name: secureInput(inputNameElement.value),
+    date: myDate(new Date),
+    text: secureInput(textareaElement.value),
+    likes: 0,
+    isLike: false,
+    forceError: false,
+    token
+  }).then(() => {
+    return getFetchPromise();
+  })
+}
 
 export const renderComments = () => {
   const commentHtml = comments.map((comment, index) => {
@@ -114,8 +114,8 @@ export const renderComments = () => {
     </div>
   </div>
 </div>`;
-  
-  if(!token) {
+
+  if (!token) {
     const appHtml = `<section id="loaderComments" class="loader -display-none">
     <h4 id="loaderText" class="text-loader">Комментарии загружаются...</h4>
   </section>
@@ -145,132 +145,132 @@ export const renderComments = () => {
       </div>
     </div>`;
 
-  appEl.innerHTML = appHtml;
+    appEl.innerHTML = appHtml;
 
-document.querySelector('.login-form-button').addEventListener('click', () => {
-  token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-  getFetchPromise();
-  //renderComments();
-})
+    document.querySelector('.login-form-button').addEventListener('click', () => {
+      token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+      getFetchPromise();
+      //renderComments();
+    })
 
-  return;
+    return;
   }
 
 
   appEl.innerHTML = appHtml;
 
 
-//   const appHtml = `
-//   <section id="loaderComments" class="loader -display-none">
-//     <h4 id="loaderText" class="text-loader">Комментарии загружаются...</h4>
-//   </section>
-// <div class="container">
-//   <ul class="comments">
-//   <!-- список рендерится из js !!!!!!!-->
-//   ${commentHtml}
-//   </ul>
-//   <div class="add-form">
-//     <input
-//       type="text"
-//       class="add-form-name"
-//       placeholder="Введите ваше имя"
-//       value=""
-//     />
-//     <textarea
-//       type="textarea"
-//       class="add-form-text"
-//       placeholder="Введите ваш комментарий"
-//       rows="4"
-//       value = ""
-//     ></textarea>
-//     <div class="add-form-row">
-//       <button class="add-form-button">Написать</button>
-//       <button class="add-form-button add-form-button--remove">Удалить последний комментарий</button>
-//     </div>
-//   </div>
-// </div>`;
-
-    
-  
-    // appEl.innerHTML = appHtml;
-    
-
-    const buttonElement = document.querySelector('button.add-form-button');
-    const listElement = document.querySelector('.comments');
-    const inputNameElement = document.querySelector('.add-form-name');
-    const textareaElement = document.querySelector('.add-form-text'); //export const loaderCommentsElement = document.getElementById('loaderComments');
-    const addFormElement = document.querySelector('.add-form');
+  //   const appHtml = `
+  //   <section id="loaderComments" class="loader -display-none">
+  //     <h4 id="loaderText" class="text-loader">Комментарии загружаются...</h4>
+  //   </section>
+  // <div class="container">
+  //   <ul class="comments">
+  //   <!-- список рендерится из js !!!!!!!-->
+  //   ${commentHtml}
+  //   </ul>
+  //   <div class="add-form">
+  //     <input
+  //       type="text"
+  //       class="add-form-name"
+  //       placeholder="Введите ваше имя"
+  //       value=""
+  //     />
+  //     <textarea
+  //       type="textarea"
+  //       class="add-form-text"
+  //       placeholder="Введите ваш комментарий"
+  //       rows="4"
+  //       value = ""
+  //     ></textarea>
+  //     <div class="add-form-row">
+  //       <button class="add-form-button">Написать</button>
+  //       <button class="add-form-button add-form-button--remove">Удалить последний комментарий</button>
+  //     </div>
+  //   </div>
+  // </div>`;
 
 
-      buttonElement.addEventListener('click', () => {
-        inputNameElement.classList.remove('error');
-        textareaElement.classList.remove('error')
-        if (!inputNameElement.value || !textareaElement.value) {
-          inputNameElement.classList.add('error');
-          textareaElement.classList.add('error');
-          return;
-        };
-      
-        buttonElement.disabled = true;
-        buttonElement.textContent = "Добавляется..."
-        addFormElement.classList.add('-display-block')
-        console.log(addFormElement);
-        postFetchPromise().then((response) => {
-        buttonElement.disabled = false;
-          buttonElement.textContent = "Написать"
-          inputNameElement.value = '';
-          textareaElement.value = '';
-          return response
-        }).catch((error) => {
-          addFormElement.classList.remove('-display-block')
-          buttonElement.disabled = false;
-          buttonElement.textContent = 'Написать';
-          if(!navigator.onLine) {
-            alert('Кажется, у вас сломался интернет, попробуйте позже')
-            // throw new Error("Сломался интернет")
-          }
-          console.warn(error);
-      });
-        renderComments(listElement);
-        buttonElement.disabled = true;
-        addFormElement.classList.remove('-display-block')
-      });
-     
-    
-    const validateButton = () => {
-      if (!inputNameElement.value || !textareaElement.value) {
-        buttonElement.disabled = true;
-      } else buttonElement.disabled = false;
-    }
 
-    const initDisabledButtonElement = () => {
-      validateButton();
-      document.querySelectorAll(".add-form-text,.add-form-name").forEach((input) => {
-        input.addEventListener("input", () => {
-          validateButton();
-        });
-      });
+  // appEl.innerHTML = appHtml;
+
+
+  const buttonElement = document.querySelector('button.add-form-button');
+  const listElement = document.querySelector('.comments');
+  const inputNameElement = document.querySelector('.add-form-name');
+  const textareaElement = document.querySelector('.add-form-text'); //export const loaderCommentsElement = document.getElementById('loaderComments');
+  const addFormElement = document.querySelector('.add-form');
+
+
+  buttonElement.addEventListener('click', () => {
+    inputNameElement.classList.remove('error');
+    textareaElement.classList.remove('error')
+    if (!inputNameElement.value || !textareaElement.value) {
+      inputNameElement.classList.add('error');
+      textareaElement.classList.add('error');
+      return;
     };
 
-    const pushEnter = () => {
-      addFormElement.addEventListener('keyup', (ent) => {
-            if (ent.code === "Enter") {
-              buttonElement.click();
-              inputNameElement.value = '';
-              textareaElement.value = '';
-            }
-          });
-       }
+    buttonElement.disabled = true;
+    buttonElement.textContent = "Добавляется..."
+    addFormElement.classList.add('-display-block')
+    console.log(addFormElement);
+    postFetchPromise().then((response) => {
+      buttonElement.disabled = false;
+      buttonElement.textContent = "Написать"
+      inputNameElement.value = '';
+      textareaElement.value = '';
+      return response
+    }).catch((error) => {
+      addFormElement.classList.remove('-display-block')
+      buttonElement.disabled = false;
+      buttonElement.textContent = 'Написать';
+      if (!navigator.onLine) {
+        alert('Кажется, у вас сломался интернет, попробуйте позже')
+        // throw new Error("Сломался интернет")
+      }
+      console.warn(error);
+    });
+    renderComments(listElement);
+    buttonElement.disabled = true;
+    addFormElement.classList.remove('-display-block')
+  });
 
-    pushEnter();
-    //pushNewComment();
-    //initChangeLikeButtonListeners();
-    //initEditButtonListeners();
-    initDisabledButtonElement();
-    //deleteComment();
-    //answerToComment(); ДЗ 2.11
-    //answerQuoteToComment(); // ДЗ со звездочкой 2.11
+
+  const validateButton = () => {
+    if (!inputNameElement.value || !textareaElement.value) {
+      buttonElement.disabled = true;
+    } else buttonElement.disabled = false;
   }
+
+  const initDisabledButtonElement = () => {
+    validateButton();
+    document.querySelectorAll(".add-form-text,.add-form-name").forEach((input) => {
+      input.addEventListener("input", () => {
+        validateButton();
+      });
+    });
+  };
+
+  const pushEnter = () => {
+    addFormElement.addEventListener('keyup', (ent) => {
+      if (ent.code === "Enter") {
+        buttonElement.click();
+        inputNameElement.value = '';
+        textareaElement.value = '';
+      }
+    });
+  }
+
+  pushEnter();
+  //pushNewComment();
+  //initChangeLikeButtonListeners();
+  //initEditButtonListeners();
+  initDisabledButtonElement();
+  //deleteComment();
+  //answerToComment(); ДЗ 2.11
+  //answerQuoteToComment(); // ДЗ со звездочкой 2.11
+}
 
 //getFetchPromise();
 renderComments(listElement)
@@ -278,42 +278,19 @@ renderComments(listElement)
 
 //Функция POST
 
-export const postFetchPromise = (
-  //{name, date, text, forceError, token}
-  ) => {
-  
-  return fetch(host, {
-    method: "POST",
-    body: JSON.stringify({
-      name: secureInput(inputNameElement.value),
-      date: myDate(new Date),
-      text: secureInput(textareaElement.value),
-      likes: 0,
-      isLike: false,
-      forceError: false,
-      // name,
-      // date,
-      // text,
-      // forceError,
-    }),
-    headers: {
-      Authorization: token,
-  }
-  }).then((response) => {
-
-    if (response.status === 201){
-       return response.json();
-    } else if (response.status === 500) {
-      alert('Сервер не работает, повторите попытку позже')
-      throw new Error("Сервер сломался, попробуй позже")
-     } else if (response.status === 400) {
-      alert ('Имя и комментарий должны быть не короче 3 символов')
-      throw new Error("Имя и комментарий короче 3 символов")
-    }  
-  }).then(() => {
-    return getFetchPromise();
-  })
-}
+// export const postFetchPromise = () => {
+//   addComment({
+//     name: secureInput(inputNameElement.value),
+//     date: myDate(new Date),
+//     text: secureInput(textareaElement.value),
+//     likes: 0,
+//     isLike: false,
+//     forceError: false,
+//     token
+//   }).then(() => {
+//     return getFetchPromise();
+//   })
+// }
 
 //validateButton()
 //initDisabledButtonElement()
