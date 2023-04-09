690c25a5434751bccd5d4c2ad6a97885cdfd9c10
Nataiia Vodopianova <natalvod@yandex.ru>
2023-04-09 23:53:21 +0300

    добавляет вызов апи логина

--- api.js
+++ api.js
@@ -41,4 +41,26 @@ export function addComment({ name, text, date, forceError, token }) {
             throw new Error("Имя и комментарий короче 3 символов")
         }
     });
+};
+
+//https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md
+export function loginUser({ login, password }) {
+    return fetch('https://webdev-hw-api.vercel.app/api/user/login', {
+        method: "POST",
+        body: JSON.stringify({
+            login,
+            password,
+        }),
+    }).then((response) => {
+
+        if (response.status === 201) {
+            return response.json();
+        } else if (response.status === 500) {
+            alert('Сервер не работает, повторите попытку позже')
+            throw new Error("Сервер сломался, попробуй позже")
+        } else if (response.status === 400) {
+            alert('Имя и комментарий должны быть не короче 3 символов')
+            throw new Error("Имя и комментарий короче 3 символов")
+        }
+    });
 }
--- components/login-component.js
+++ components/login-component.js
@@ -1,4 +1,6 @@
-   export function renderLoginComponent({ appEl, commentHtml, setToken, getFetchPromise }) {
+import { loginUser } from "../api.js";
+
+export function renderLoginComponent({ appEl, commentHtml, setToken, getFetchPromise }) {
     const appHtml = `
     <section id="loaderComments" class="loader -display-none">
     <h4 id="loaderText" class="text-loader">Комментарии загружаются...</h4>
@@ -32,8 +34,14 @@
     appEl.innerHTML = appHtml;
 
     document.querySelector('.login-form-button').addEventListener('click', () => {
-      setToken("Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k")
-      getFetchPromise();
-      //renderComments();
-    })
-   }
+        // setToken("Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k");
+        loginUser({
+            login: 'admin',
+            password: 'admin',
+        }).then((user) => {
+            // console.log(user);
+            setToken(`Bearer ${user.user.token}`);
+            getFetchPromise();
+        });
+    });
+}
