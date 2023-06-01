418a0a0b67970a2a291d3024b991cdd422ec9432
DmitryTell <dmitrij.teleganov@gmail.com>
2023-06-01 19:49:04 +0300

    Start working

--- api.js
+++ api.js
@@ -1,70 +1,75 @@
-import {
-    commentList,
-    startingElement,
-    addFormElement,
-    waitingElement,
-    nameElement,
-    textElement,
-    addButton
-} from "./data.js";
-import renderComments from "./renderComments.js";
-import { replaceValue } from "./handleFunctions.js";
 
-const startFetch = (element) => {
-    fetch("https://webdev-hw-api.vercel.app/api/v1/dmitry-teleganov/comments", {
-        method: "GET"
-    })
-    .then(response => response.json())
-    .then(responseData => {
-        const comments = responseData.comments;
-        commentList.classList.remove('hidden');
-        startingElement.classList.add('hidden');
-        addButton.disabled = true;
 
-        renderComments(element, comments);
-    });
-}
 
-const handlePostClick = (element) => {
-    return fetch("https://webdev-hw-api.vercel.app/api/v1/dmitry-teleganov/comments", {
-        method: "POST",
-        body: JSON.stringify({
-        name: replaceValue(nameElement.value),
-        text: replaceValue(textElement.value)
-          .replaceAll('START_QUOTE', '<div class="comment-quote">')
-          .replaceAll('END_QUOTE', '</div>'),
-        forceError: true
-        })
-    })
-    .then(response => {
-        if (response.status === 400) throw new Error('Ошибка 400');
-        if (response.status === 500) throw new Error('Ошибка 500');
+
+
+// import {
+//     commentList,
+//     startingElement,
+//     addFormElement,
+//     waitingElement,
+//     nameElement,
+//     textElement,
+//     addButton
+// } from "./data.js";
+// import renderComments from "./renderComments.js";
+// import { replaceValue } from "./handleFunctions.js";
+
+// const startFetch = (element) => {
+//     fetch("https://webdev-hw-api.vercel.app/api/v1/dmitry-teleganov/comments", {
+//         method: "GET"
+//     })
+//     .then(response => response.json())
+//     .then(responseData => {
+//         const comments = responseData.comments;
+//         commentList.classList.remove('hidden');
+//         startingElement.classList.add('hidden');
+//         addButton.disabled = true;
+
+//         renderComments(element, comments);
+//     });
+// }
+
+// const handlePostClick = (element) => {
+//     return fetch("https://webdev-hw-api.vercel.app/api/v1/dmitry-teleganov/comments", {
+//         method: "POST",
+//         body: JSON.stringify({
+//         name: replaceValue(nameElement.value),
+//         text: replaceValue(textElement.value)
+//           .replaceAll('START_QUOTE', '<div class="comment-quote">')
+//           .replaceAll('END_QUOTE', '</div>'),
+//         forceError: true
+//         })
+//     })
+//     .then(response => {
+//         if (response.status === 400) throw new Error('Ошибка 400');
+//         if (response.status === 500) throw new Error('Ошибка 500');
   
-        return response.json();
-    })
-    .then(() => {
-        nameElement.value = '';
-        textElement.value = '';
-        addFormElement.classList.remove('hidden');
-        waitingElement.classList.add('hidden');
+//         return response.json();
+//     })
+//     .then(() => {
+//         nameElement.value = '';
+//         textElement.value = '';
+//         addFormElement.classList.remove('hidden');
+//         waitingElement.classList.add('hidden');
   
-        return startFetch(element);
-    })
-    .catch(error => {
-        if (error.message === 'Ошибка 400') {
-            alert('Имя и комментарий должны быть не короче 3 символов');
-            addFormElement.classList.remove('hidden');
-            waitingElement.classList.add('hidden');
-        } else if (error.message === 'Ошибка 500') {
-            alert('Сервер сломался, попробуй позже');
+//         return startFetch(element);
+//     })
+//     .catch(error => {
+//         if (error.message === 'Ошибка 400') {
+//             alert('Имя и комментарий должны быть не короче 3 символов');
+//             addFormElement.classList.remove('hidden');
+//             waitingElement.classList.add('hidden');
+//         } else if (error.message === 'Ошибка 500') {
+//             alert('Сервер сломался, попробуй позже');
   
-            handlePostClick(element);
-        } else {
-            alert('Кажется, у вас сломался интернет, попробуйте позже');
-            addFormElement.classList.remove('hidden');
-            waitingElement.classList.add('hidden');
-        }
-    });
-}
+//             handlePostClick(element);
+//         } else {
+//             alert('Кажется, у вас сломался интернет, попробуйте позже');
+//             addFormElement.classList.remove('hidden');
+//             waitingElement.classList.add('hidden');
+//         }
+//     });
+// }
 
-export { startFetch, handlePostClick };
+// export { startFetch, handlePostClick };
--- main.js
+++ main.js
@@ -1,64 +1,7 @@
 "use strict";
 
-import {
-  commentList,
-  startingElement,
-  addFormElement,
-  waitingElement,
-  nameElement,
-  textElement,
-  addButton
-} from "./data.js";
-import { startFetch, handlePostClick } from "./api.js";
+const appElement = document.querySelector('#app');
 
-startingElement.classList.remove('hidden');
-commentList.classList.add('hidden');
-
-startFetch(commentList);
-
-addButton.addEventListener('click', () => {
-  if (!nameElement.value.trim()) {
-    nameElement.classList.add('form-error');
-    return;
-  } else if (!textElement.value.trim()) {
-    textElement.classList.add('form-error');
-    return;
-  } else {
-    waitingElement.classList.remove('hidden');
-    addFormElement.classList.add('hidden');
-
-    handlePostClick(commentList);
-  }
-
-  nameElement.classList.remove('form-error');
-  textElement.classList.remove('form-error');
-});
-textElement.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    if (!nameElement.value.trim()) {
-      nameElement.classList.add('form-error');
-      return;
-    } else if (!textElement.value.trim()) {
-      textElement.classList.add('form-error');
-      return;
-    } else {
-      waitingElement.classList.remove('hidden');
-      addFormElement.classList.add('hidden');
-  
-      handlePostClick(commentList);
-    }
-  
-    nameElement.classList.remove('form-error');
-    textElement.classList.remove('form-error');
-  }
-});
-nameElement.addEventListener('input', () => {
-  if (textElement.value.trim()) {
-    addButton.disabled = false;
-  }
-});
-textElement.addEventListener('input', () => {
-  if (nameElement.value.trim()) {
-    addButton.disabled = false;
-  }
-});
+let comments = [];
+let token = null;
+let isStarting = true;
