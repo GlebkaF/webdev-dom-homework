317a5868ab7160359095e4649bb13794ac03e3b7
Olya-Myalo <olyamyalo@mail.ru>
2023-06-03 17:10:35 +0300

    module api is ready

--- api.js
+++ api.js
@@ -1,33 +1,41 @@
-// import comments from "./main.js";
-// import renderComments from "./main.js";
+import { formatDate } from "./utils.js";
+export const fetchComments = () => {
+   return fetch("https://webdev-hw-api.vercel.app/api/v1/olya-myalo/comments", {
+      method: "GET"
+    })
+    .then((response) => (response.json()))
+    .then((responseData) => { 
+    const appComments = responseData.comments.map((comment) => { 
+      return { 
+        name: comment.author.name,  
+        date: formatDate(new Date(comment.date)),  
+        text: comment.text, 
+        active: false,
+        like: comment.likes, 
+    }; 
+    }); 
+    return appComments;
+    });
+}
 
-// const commentsContainer = document.getElementById("commentsContainer");
-// commentsContainer.style.display = 'none';
-// const commentAdding = document.createElement('div'); 
-// commentAdding.innerText = 'Пожалуйста подождите, загружаю комментарии...';
-// commentsContainer.parentNode.insertBefore(commentAdding, commentsContainer);
-// const fetchPromise = () => {
-//     fetch("https://webdev-hw-api.vercel.app/api/v1/olya-myalo/comments", {
-//       method: "GET"
-//     })
-//     .then((response) => (response.json()))
-//     .then((responseData) => { 
-//     const appComments = responseData.comments.map((comment) => { 
-//       return { 
-//         name: comment.author.name,  
-//         date: formatDate(new Date(comment.date)),  
-//         text: comment.text, 
-//         active: false,
-//         like: comment.likes, 
-//         }; 
-//       }); 
-//       comments = appComments; 
-//       renderComments();
-//       })
-//       .then((data) => {
-//         commentAdding.style.display = 'none';
-//         commentsContainer.style.display = 'block';
-//         });
-// };
-
-// export default fetchPromise;
+export const createComment = (name, text) => {
+  return  fetch("https://webdev-hw-api.vercel.app/api/v1/olya-myalo/comments", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,  
+          text: text,
+          forceError: false,
+        })
+      })
+      .then((response) => {
+        if (response.status === 500) {
+          throw new Error("Сервер упал");
+        }
+        if (response.status === 400) {
+          throw new Error("Неверный запрос");
+        }
+        else {
+          return response.json();
+        }
+      })
+}
---
+++ renderComments.js
@@ -0,0 +1,51 @@
+const getListCommentsEdit = (comment, index) => {
+  let likeClass = comment.active ? "-active-like" : "";
+     return `<li class="comment">
+           <div class="comment-header">
+             <div id ="name">${comment.name}</div>
+             <div>${comment.date} </div>
+           </div>
+           <div class="comment-body">
+             <div id = "text" data-index="${index}" class="comment-text" style={{ whiteSpace: 'pre-line' }}>
+               ${comment.text}
+             </div>
+           </div>
+           <div class="comment-footer">
+             <button data-index="${index}" class="edited-button">Редактировать</button>
+             <div class="likes">
+               <span class="likes-counter">${comment.like}</span> ${comment.like === 1 ? 'лайк' : 'лайков'}
+               <button data-index="${index}" class="like-button ${likeClass}"></button> 
+             </div>
+           </div>
+         </li>`
+}
+
+export const renderComments = (comments, handleCommentEditClick, handleCommentFeedbackClick, handleCommentLikeClick) => {
+    const commentsHtml = comments.map((comment, index) => getListCommentsEdit(comment, index))
+     .join('');
+const listElement = document.getElementById("list");
+     listElement.innerHTML = commentsHtml;
+    setTimeout(() => {
+      onCommentLikeClick(handleCommentLikeClick);
+      onCommentEditClick(handleCommentEditClick);
+      onCommentFeedbackClick(handleCommentFeedbackClick)
+    }, 1000) 
+   }
+export const onCommentLikeClick = (handleCommentLikeClick) => {
+    const likeButtonsElements = document.querySelectorAll(".like-button");
+    for (const likeButtonsElement of likeButtonsElements) {
+      likeButtonsElement.addEventListener("click", handleCommentLikeClick);
+    }
+  };
+export const onCommentEditClick = (handleCommentEditClick) => { 
+    const editButtonsElements = document.querySelectorAll(".edited-button"); 
+    for (const editButtonElement of editButtonsElements) { 
+      editButtonElement.addEventListener("click", handleCommentEditClick);
+    } 
+  };
+export const onCommentFeedbackClick = (handleCommentFeedbackClick) => {  
+    const feedbackButtonsElements = document.querySelectorAll(".comment-text");   
+    for (const feedbackButtonsElement of feedbackButtonsElements) {   
+      feedbackButtonsElement.addEventListener("click", handleCommentFeedbackClick);  
+    }  
+  };
