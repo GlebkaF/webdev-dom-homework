42cca4933e5261572f056de8caa09c01fa3eb0d4
«Marina» <marina_obruch@mail.ru>
2023-06-04 09:17:58 +0500

    smal fiixes

--- script.js
+++ script.js
@@ -146,6 +146,8 @@ const renderComments = () => {
             }
             constWaitingComment.classList.remove('hidden');
             addCommentForm.classList.add('hidden');
+            token =
+                "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
             postComment();
         }
         clickButton();
