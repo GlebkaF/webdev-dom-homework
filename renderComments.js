abd472d1168d65f178daf0f590af17906c22973d
«Marina» <marina_obruch@mail.ru>
2023-06-04 19:30:18 +0500

    Добавление динамической формы-стартовая стрюсайта

--- renderComments.js
+++ renderComments.js
@@ -8,19 +8,56 @@ const commentInputElement = document.querySelector(".add-form-text");
 const constWaitingComment = document.querySelector('.add-waiting'); // Комментарий добавляется...
 
 
-// Функция render
-const renderComments = (isLoading, isWaitingComment, comments) => {
 
-  // Лоадинг на загрузку комментариев на страницу
-  if (isLoading) {
-    document.getElementById('comments').innerHTML =
-      'Пожалуйста подождите, загружаю комментарии...';
-    constWaitingComment.classList.add(`hidden`);
-    return;
-  }
+// // ОСТАВИТЬ
+//     const ///// = `<div class="container">
+//             <ul id="comments" class="comments">
+//               ${commentHTML}
+//             </ul>
+//             <div class="add-form">
+//               <input type="text" id="add-form-name" class="add-form-name" placeholder="Введите ваше имя" />
+
+//               <textarea type="textarea" id="add-form-text" class="add-form-text" placeholder="Введите ваш коментарий"
+//                 rows="4"></textarea>
+
+//               <div class="add-form-row">
+//                 <button type="button" id="add-form-button" class="add-form-button" disabled>Написать</button>
+//                 <button class="remove-form-button">Удалить последний</button>
+//               </div>
+//             </div>
+//             <p class="add-waiting hidden">Комментарий добавляется...</p>
+//           </div>
+
+//           <div class="registration">
+//           <div class="add-form">
+//             <h3>Форма ввода</h3>
+//             <div class="reg-input">
+//               <input type="text" id="add-login" class="add-login" placeholder="Введите логин" />
+//               <input type="text" id="add-password" class="add-password" placeholder="Введите пароль"></input>
+//             </div>
+//             <div class="add-reg-form">
+//               <button type="button" id="in-button" class="in-button">Войти</button>
+//               <button class="reg-button">Зарегистрироваться</button>
+//             </div>
+//           </div>
+//         </div>
+//           `
+
+//     appEl.innerHTML = /////;
+
+// Функция render
+const renderComments = (app, isLoading, isWaitingComment, comments) => {
+
+  // // Лоадинг на загрузку комментариев на страницу
+  // if (isLoading) {
+  //   document.getElementById('comments').innerHTML =
+  //     'Пожалуйста подождите, загружаю комментарии...';
+  //   constWaitingComment.classList.add(`hidden`);
+  //   return;
+  // }
 
   // Рендер
-  listOfComments.innerHTML = comments
+  const commentHTML = comments
     .map((comment, index) => {
       return `<li id="comment" class="comment" data-index="${index}">
       <div class="comment-header">
@@ -44,17 +81,31 @@ const renderComments = (isLoading, isWaitingComment, comments) => {
     </li>`
     }).join("");
 
-  // Функция лоадинг при добавлении комментариев в ленту
-  const waitingAddComment = () => {
-    if (isWaitingComment) {
-      constWaitingComment.classList.remove(`hidden`);
-      addCommentForm.classList.add(`hidden`);
-    } else {
-      constWaitingComment.classList.add(`hidden`);
-      addCommentForm.classList.remove(`hidden`);
+  const appHtml = `
+  <div class="container">
+          <ul id="comments" class="comments">
+
+      ${isLoading
+      ? "<p class='add-waiting hidden'>Комментарий добавляется...</p>"
+      : commentHTML
     }
-  };
-  waitingAddComment();
+          </ul >
+  <div class="form-loaging" style="margin-top: 20px">
+    Чтобы добавить комментарий, <a href=" " id="go-to-login">Авторизуйтесь</a>
+  </div>`;
+  app.innerHTML = appHtml;
+
+  // Функция лоадинг при добавлении комментариев в ленту
+  // const waitingAddComment = () => {
+  //   if (isWaitingComment) {
+  //     constWaitingComment.classList.remove(`hidden`);
+  //     addCommentForm.classList.add(`hidden`);
+  //   } else {
+  //     constWaitingComment.classList.add(`hidden`);
+  //     addCommentForm.classList.remove(`hidden`);
+  //   }
+  // };
+  // waitingAddComment();
 
 
   // Добавление клика на лайк
@@ -69,7 +120,7 @@ const renderComments = (isLoading, isWaitingComment, comments) => {
         let comment = comments[likeButtonsElement.dataset.index];
         comment.isLikeLoading = true;
 
-        renderComments(isLoading, isWaitingComment, comments);
+        renderComments(app, isLoading, isWaitingComment, comments);
 
         // Инициализация задержки при обработке лайка на комментарий
         delay(2000).then(() => {
@@ -81,7 +132,7 @@ const renderComments = (isLoading, isWaitingComment, comments) => {
 
           comment.isLiked = !comment.isLiked;
           comment.isLikeLoading = false;
-          renderComments(isLoading, isWaitingComment, comments);
+          renderComments(app, isLoading, isWaitingComment, comments);
         });
       });
     }
@@ -98,35 +149,35 @@ const renderComments = (isLoading, isWaitingComment, comments) => {
         let index = element.dataset.index;
 
         commentInputElement.value = `START_QUOTE${comments[index].author.name}:
-          \n${comments[index].text.replaceAll('<div class="comment-quote">', 'START_QUOTE').replaceAll('</div>', 'END_QUOTE')}END_QUOTE`;
+\n${comments[index].text.replaceAll('<div class="comment-quote">', 'START_QUOTE').replaceAll('</div>', 'END_QUOTE')} END_QUOTE`;
       });
     }
   }
   answerComment();
 
-  // валидация на ввод (неактивная кнопка "Написать")
-  const checkAddButton = () => {
-    nameInputElement.addEventListener('input', () => {
-      if (nameInputElement.value) {
-        buttonElement.disabled = false;
-        return;
-      } else {
-        buttonElement.disabled = true;
-        return;
-      }
-    });
+  // // валидация на ввод (неактивная кнопка "Написать")
+  // const checkAddButton = () => {
+  //   nameInputElement.addEventListener('input', () => {
+  //     if (nameInputElement.value) {
+  //       buttonElement.disabled = false;
+  //       return;
+  //     } else {
+  //       buttonElement.disabled = true;
+  //       return;
+  //     }
+  //   });
 
-    commentInputElement.addEventListener('input', () => {
-      if (commentInputElement.value) {
-        buttonElement.disabled = false;
-        return;
-      } else {
-        buttonElement.disabled = true;
-        return;
-      }
-    });
-  }
-  checkAddButton();
+  //   commentInputElement.addEventListener('input', () => {
+  //     if (commentInputElement.value) {
+  //       buttonElement.disabled = false;
+  //       return;
+  //     } else {
+  //       buttonElement.disabled = true;
+  //       return;
+  //     }
+  //   });
+  // }
+  // checkAddButton();
 }
 
 export { renderComments };
--- script.js
+++ script.js
@@ -1,48 +1,12 @@
 import { fetchAndRenderTasks, postComment } from "/api.js";
 import { renderComments } from "/renderComments.js";
 
-
-// // ОСТАВИТЬ
-//     const appHtml = `<div class="container">
-//             <ul id="comments" class="comments">
-//               ${commentHTML}
-//             </ul>
-//             <div class="add-form">
-//               <input type="text" id="add-form-name" class="add-form-name" placeholder="Введите ваше имя" />
-
-//               <textarea type="textarea" id="add-form-text" class="add-form-text" placeholder="Введите ваш коментарий"
-//                 rows="4"></textarea>
-
-//               <div class="add-form-row">
-//                 <button type="button" id="add-form-button" class="add-form-button" disabled>Написать</button>
-//                 <button class="remove-form-button">Удалить последний</button>
-//               </div>
-//             </div>
-//             <p class="add-waiting hidden">Комментарий добавляется...</p>
-//           </div>
-
-//           <div class="registration">
-//           <div class="add-form">
-//             <h3>Форма ввода</h3>
-//             <div class="reg-input">
-//               <input type="text" id="add-login" class="add-login" placeholder="Введите логин" />
-//               <input type="text" id="add-password" class="add-password" placeholder="Введите пароль"></input>
-//             </div>
-//             <div class="add-reg-form">
-//               <button type="button" id="in-button" class="in-button">Войти</button>
-//               <button class="reg-button">Зарегистрироваться</button>
-//             </div>
-//           </div>
-//         </div>
-//           `
-
-//     appEl.innerHTML = appHtml;
-
 // Определение переменных
 
 const buttonElement = document.querySelector(".add-form-button");
 const nameInputElement = document.querySelector(".add-form-name");
 const commentInputElement = document.querySelector(".add-form-text");
+const app = document.getElementById("app");
 
 
 // Данные о комментариях и маркеры для лоадингов/загрузки
@@ -57,7 +21,7 @@ fetchAndRenderTasks()
         isWaitingComment = false;
         isLoading = false;
 
-        renderComments(isLoading, isWaitingComment, comments);
+        renderComments(app, isLoading, isWaitingComment, comments);
     });
 
 fetchAndRenderTasks();
@@ -80,14 +44,14 @@ buttonElement.addEventListener('click', () => {
 
     // Инициализация лоадинга при добавлении комментария
     isWaitingComment = true;
-    renderComments(isLoading, isWaitingComment, comments);
+    renderComments(app, isLoading, isWaitingComment, comments);
 
     postComment(nameInputElement, commentInputElement)
         .then(() => {
             nameInputElement.value = "";
             commentInputElement.value = "";
             isWaitingComment = false;
-            renderComments(isLoading, isWaitingComment, comments);
+            renderComments(app, isLoading, isWaitingComment, comments);
         })
         .catch((error) => {
             if (error.message === "Ошибка 400") {
@@ -103,11 +67,11 @@ buttonElement.addEventListener('click', () => {
 
             else {
                 isWaitingComment = false;
-                renderComments(isLoading, isWaitingComment, comments);
+                renderComments(app, isLoading, isWaitingComment, comments);
                 alert("Кажется, у вас сломался интернет, попробуйте позже");
             }
         });
-    renderComments(isLoading, isWaitingComment, comments);
+    renderComments(app, isLoading, isWaitingComment, comments);
 });
 
-renderComments(isLoading, isWaitingComment, comments);
+renderComments(app, isLoading, isWaitingComment, comments);
