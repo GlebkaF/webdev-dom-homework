95095a04caa9624e66ba91a70335154eebd86837
Alex-Viet <alex.night.power@gmail.com>
2023-06-10 04:00:55 +0300

    Добавлена динамическая форма входа

--- JS/renderApp.js
+++ JS/renderApp.js
@@ -1,9 +1,15 @@
 import { likeButtonsListeners } from "./likes.js";
-import { isInitialLoading, isPosting, token } from "./api.js";
-import { postToApi } from "./api.js";
+import { isInitialLoading, isPosting } from "./api.js"; // ВЕРНУТЬ TOKEN!
+import { getFromApi, postToApi } from "./api.js";
 import { getApp, getAuthForm, getCommentsAndAuth } from "./appHtml.js";
 
 export const appElement = document.getElementById('app');
+
+
+
+
+let token = null;
+// "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
 
 
 const renderApp = (data, elem, getList) => {
@@ -34,90 +40,109 @@ const renderApp = (data, elem, getList) => {
         authLink.addEventListener("click", () => {
             appHtml = getAuthForm();
             appElement.innerHTML = appHtml;
-        });
 
-    } else {
-        appHtml = getApp(commentsHtml);
-        appElement.innerHTML = appHtml;
+            const loginBtn = document.getElementById('login-button');
 
-        const commentBtn = document.getElementById('form-add-button');
-        const addName = document.getElementById('add-name');
-        const addComment = document.getElementById('add-comment');
-        const loadingComments = document.querySelector('.loading');
-        const addFormElem = document.querySelector('.add-form');
-        const commentAddedElem = document.querySelector('.comment-added');
-
-        isInitialLoadingFunc(loadingComments);
-
-        if (isPosting) {
-            addFormElem.style.display = 'none';
-            commentAddedElem.style.display = 'block';
-        } else {
-            addFormElem.style.display = 'block';
-            commentAddedElem.style.display = 'none';
-        };
-
-        likeButtonsListeners();
+            // - Добавить авторизацию по логину и паролю! Пока авторизация происходит без каких-либо данных
+            // + Связать авторизацию с токеном, после аутентификации должна загружаться рабочая страница
+            // - Добавить форму регистрации и кнопку-toggle
+            // - Обернуть (appHtml = функция вызова разметки(); appElement.innerHTML = appHtml;) в функцию
+            // - В конце не забыть убрать все доп функции в отдельный модуль
 
 
-        const addNewComment = () => {
-            commentBtn.addEventListener("click", () => {
-                addName.classList.remove("error");
-                addComment.classList.remove("error");
-
-                if (addName.value.trim() === '') {
-                    return addName.classList.add("error");
-                } else if (addComment.value.trim() === '') {
-                    return addComment.classList.add("error");
-                };
-
-
-                renderApp(data, elem, getList);
-                postToApi(data, addComment, addName);
+            loginBtn.addEventListener("click", () => {
+                // appHtml = getApp(commentsHtml);
+                // appElement.innerHTML = appHtml;
+                token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+                getFromApi(data);
             });
-        };
+        });
+        return;
+    };
 
-        addNewComment();
 
 
-        function checkInputs() {
-            let isFilled = true;
+    appHtml = getApp(commentsHtml);
+    appElement.innerHTML = appHtml;
 
-            if (addName.value.trim() === '' || addComment.value.trim() === '') {
-                isFilled = false;
-            }
+    const commentBtn = document.getElementById('form-add-button');
+    const addName = document.getElementById('add-name');
+    const addComment = document.getElementById('add-comment');
+    const loadingComments = document.querySelector('.loading');
+    const addFormElem = document.querySelector('.add-form');
+    const commentAddedElem = document.querySelector('.comment-added');
 
-            if (isFilled) {
-                commentBtn.disabled = false;
-                commentBtn.classList.remove("hidden-button");
+    isInitialLoadingFunc(loadingComments);
+
+    if (isPosting) {
+        addFormElem.style.display = 'none';
+        commentAddedElem.style.display = 'block';
+    } else {
+        addFormElem.style.display = 'block';
+        commentAddedElem.style.display = 'none';
+    };
+
+    likeButtonsListeners();
+
+
+    const addNewComment = () => {
+        commentBtn.addEventListener("click", () => {
+            addName.classList.remove("error");
+            addComment.classList.remove("error");
+
+            if (addName.value.trim() === '') {
+                return addName.classList.add("error");
+            } else if (addComment.value.trim() === '') {
+                return addComment.classList.add("error");
+            };
+
+
+            renderApp(data, elem, getList);
+            postToApi(data, addComment, addName);
+        });
+    };
+
+    addNewComment();
+
+
+    function checkInputs() {
+        let isFilled = true;
+
+        if (addName.value.trim() === '' || addComment.value.trim() === '') {
+            isFilled = false;
+        }
+
+        if (isFilled) {
+            commentBtn.disabled = false;
+            commentBtn.classList.remove("hidden-button");
+        } else {
+            commentBtn.disabled = true;
+            commentBtn.classList.add("hidden-button");
+        }
+    };
+
+
+    const addNewCommentOnEnter = (event) => {
+        if (event.keyCode === 13) {
+            addName.classList.remove("error");
+            addComment.classList.remove("error");
+
+            if (addName.value.trim() === '') {
+                return addName.classList.add("error");
+            } else if (addComment.value.trim() === '') {
+                return addComment.classList.add("error");
             } else {
-                commentBtn.disabled = true;
-                commentBtn.classList.add("hidden-button");
-            }
-        };
-
-
-        const addNewCommentOnEnter = (event) => {
-            if (event.keyCode === 13) {
-                addName.classList.remove("error");
-                addComment.classList.remove("error");
-
-                if (addName.value.trim() === '') {
-                    return addName.classList.add("error");
-                } else if (addComment.value.trim() === '') {
-                    return addComment.classList.add("error");
-                } else {
-                    commentBtn.click();
-                };
+                commentBtn.click();
             };
         };
-
-
-        addName.addEventListener('input', checkInputs);
-        addName.addEventListener('keyup', addNewCommentOnEnter);
-        addComment.addEventListener('input', checkInputs);
-        addComment.addEventListener('keyup', addNewCommentOnEnter);
     };
+
+
+    addName.addEventListener('input', checkInputs);
+    addName.addEventListener('keyup', addNewCommentOnEnter);
+    addComment.addEventListener('input', checkInputs);
+    addComment.addEventListener('keyup', addNewCommentOnEnter);
+
 };
 
 
