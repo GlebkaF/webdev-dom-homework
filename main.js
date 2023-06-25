75c638d4bea3b8cfe64e276d876f3145b68a607b
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-06-25 15:13:51 +0300

    added  modul rendactComment

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { fetchGet } from "./api.js";
 import { fetchPOST } from "./api.js";
 import renderComments from "./render.js";
 import { getListComments } from "./listComments.js";
-
+import { redactComments } from "./redactComments.js";
 
 
 //  Поиск элментов
@@ -17,8 +17,8 @@ const listElement = document.getElementById("list");
 const endDeleteButtonElement = document.getElementById("end-delete-button");
 const addFormElement = document.getElementById("add-form");
 const loadingElement = document.querySelector(".loading");
-
-
+const commentsElements = document.querySelectorAll(".comment");
+export {commentsElements};
 let comments = [];
 
 function getArr() {
@@ -105,18 +105,7 @@ const initlikeButtonsListeners = () => {
 };
 
 //Функция редактирования коментов
- const redactComments = () => {
-    const commentsElements = document.querySelectorAll(".comment");
-    for (const commentElement of commentsElements) {
-        commentElement.addEventListener("click", () => {
-            const index = commentElement.dataset.index;
-            console.log(comments[index].text);
-            comentInputElement.value = `QUOTE_BEGIN${comments[index].name}:\n${comments[index].text}QUOTE_END`;
-
-        })
-    }
-}
-redactComments();
+redactComments(commentsElements);
 
 export {initlikeButtonsListeners, redactComments, initiateRedact};
 
--- render.js
+++ render.js
@@ -1,11 +1,13 @@
-import {initlikeButtonsListeners, redactComments, initiateRedact} from "./main.js";
+import {initlikeButtonsListeners,  initiateRedact} from "./main.js";
+import {commentsElements} from "./main.js";
+import { redactComments } from "./redactComments.js";
 
 // Рендерим из массива разметку
 function renderComments(element, getListComments, comments) {
     const commentsHTML = comments.map((comment, index) => getListComments(comment, index)).join('');
     element.innerHTML = commentsHTML;
     initlikeButtonsListeners();
-    redactComments();
+    redactComments(commentsElements);
     initiateRedact();
 }
 export default renderComments;
