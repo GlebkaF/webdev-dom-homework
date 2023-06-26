cc72de6e48f7dc422df92f106c61e1dba6d1de37
QWitcherS <quickwitcher@gmail.com>
2023-06-26 21:20:33 +0500

    Добавление формы регистрации

--- module_js/components/login-components.js
+++ module_js/components/login-components.js
@@ -1,15 +1,31 @@
 import { getPromise, addLogin } from "../api.js";
 
-export function renderLoginComponents({ appEl, setToken, }) {
-  const appHtml = `
+export function renderLoginComponents({ appEl, setToken }) {
+  let isLoginMode = true;
+
+  const renderForm = () => {
+    const appHtml = `
   <div class="container">
       <div class="add-form" id="form">
-        <h2>Форма для авторизации</h2>
+        <h2>Форма для ${isLoginMode ? "входа" : "регистрации"}</h2>
+        ${
+          isLoginMode
+            ? ""
+            : `Имя
+        <input
+          type="text"
+          class="user__name"
+          placeholder="Введите имя"
+          id="username" /><br>`
+        }
+        
+        Логин
         <input
           type="text"
           class="user__login"
           placeholder="Введите логин"
-          id="login" />
+          id="login" /><br>
+        Пароль
         <input
           type="password"
           class="user__password"
@@ -17,31 +33,47 @@ export function renderLoginComponents({ appEl, setToken, }) {
           rows=""
           id="password"></input>
         <div class="add-form-row">
-          <button class="add-form-button" id="login-button">Войти</button>
+          <button class="add-form-button" id="login-button">${
+            isLoginMode ? "Войти" : "Зарегистрироваться"
+          }</button><br>
+
+          <button class="add-form-button" id="toggle-button">Перейти${
+            isLoginMode ? " к регистрации" : " к авторизации"
+          }</button>
         </div>`;
 
-  appEl.innerHTML = appHtml;
+    appEl.innerHTML = appHtml;
 
-  document.getElementById("login-button").addEventListener("click", () => {
-    const login = document.getElementById('login').value;
-    const password = document.getElementById('password').value
+    document.getElementById("login-button").addEventListener("click", () => {
+      const login = document.getElementById("login").value;
+      const password = document.getElementById("password").value;
 
-    if(!login) {
-      alert('Введите логин')
-      return;
-    }
+      if (!login) {
+        alert("Введите логин");
+        return;
+      }
 
-    if(!password) {
-      alert('Введите пароль')
-      return;
-    }
-    addLogin(login, password).then((user) => {
-      console.log(user);
-      console.log(user.user);
-      setToken(`Bearer ${user.user.token}`);
-      getPromise();
-    }).catch(error => {
-      alert(error.massage)
-    })
-  });
+      if (!password) {
+        alert("Введите пароль");
+        return;
+      }
+      addLogin(login, password)
+        .then((user) => {
+          console.log(user);
+          console.log(user.user);
+          setToken(`Bearer ${user.user.token}`);
+          getPromise();
+        })
+        .catch((error) => {
+          alert(error.massage);
+        });
+    });
+
+    document.getElementById("toggle-button").addEventListener("click", () => {
+      isLoginMode = !isLoginMode;
+      renderForm();
+    });
+  };
+
+  renderForm();
 }
