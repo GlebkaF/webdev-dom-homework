b102c128a6b8b7ae88f2d4050b5403960b720dc8
Mariia <miraclefrommars@gmail.com>
2023-06-27 21:30:39 +0200

    edits

--- api.js
+++ api.js
@@ -24,3 +24,34 @@ const fetchComments = () => {
   };
 
   fetchComments();
+
+
+
+  const postComment = () => {
+    let loadingComments = document.getElementById('new-comment-loader');
+    loadingComments.classList.remove('hidden');
+    let newComment = document.getElementById('new-comment-section');
+    newComment.style.display = 'none';
+      fetch("https://webdev-hw-api.vercel.app/api/v1/mariia-goppa/comments", {
+        method: "POST",
+        body: JSON.stringify({ 
+          text: commentInputElement.value, 
+          name: nameInputElement.value,
+          dates: time(),
+          likeCounter: 0,
+        }),
+      })
+      .then((response) => {
+        return fetchComments();
+      })
+      .then((response) => {
+        let loadingComments = document.getElementById('new-comment-loader');
+        loadingComments.classList.add('hidden');
+        let newComment = document.getElementById('new-comment-section');
+        newComment.style.display = '';
+      });
+    };
+
+    postComment();
+
+  
