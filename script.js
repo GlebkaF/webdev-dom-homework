8b39eca752ff00e68517ee08e88f5b6f585865a5
aleksandr-zasimov <zasimov.am@hotmail.com>
2023-07-06 22:11:56 +0300

    fix bugs

--- script.js
+++ script.js
@@ -1,217 +1,154 @@
-const commentList = document.querySelector('.comments');
-const addForm = document.querySelector('.add-form');
-const userName = document.querySelector('.add-form-name');
-const textComment = document.querySelector('.add-form-text');
-const button = document.querySelector('.add-form-button');
-let appComments = [];
+const buttonNewComment = document.querySelector('.add-form-button');
+const comment = document.querySelector('.comment');
+const boxComments = document.querySelector('.comments');
+const inputName = document.querySelector('.add-form-name');
+const textAreaComment = document.querySelector('.add-form-text');
+const boxCommentsTexts = boxComments.querySelectorAll('.comment');
+let now = new Date();
 
-//Добавляем лайк
-const addLikes = (e) => {
-  const comment = appComments[e.target.dataset.id];
-  comment.like++;
-  comment.Iliked = true;
-}
-//Удаляем лайк
-const delLikes = (e) => {
-  const comment = appComments[e.target.dataset.id];
-  comment.like--;
-  comment.Iliked = false;
-}
-//Функция проверки был ли поставлен лайк
-const initLikeClick = () => {
-  const likeClickElements = document.querySelectorAll('.likes');
-  for (const likeClickElement of likeClickElements) {
-    likeClickElement.addEventListener('click', (e) => {
-      e.stopPropagation();
-      const comment = appComments[e.target.dataset.id];
-      if (comment.Iliked) {
-        delLikes(e);
-      } else {
-        addLikes(e);
-      }
+let userComments = [];
+
+function getApi() {
+    const fetchPromise = fetch("https://webdev-hw-api.vercel.app/api/v1/alex-zasimov/comments", {
+    method: "GET"
+  });
+    
+  fetchPromise.then((response) => {
+    const jsonPromise = response.json();
+
+    // Подписываемся на результат преобразования
+    jsonPromise.then((responseData) => {
+      // получили данные и рендерим их в приложении
+      const appComments = responseData.comments.map((comment) => {
+        return {
+          name: comment.author.name,
+          date: new Date(comment.date).toLocaleString().slice(0,-3),
+          text: comment.text,
+          likes: comment.likes,
+          isLiked: false,
+        };
+      });
+      userComments = appComments;
+      console.log(userComments);
       renderComments();
     });
-  }
-};
-//Получение комментариев из API
-const fetchPromise = fetch('https://webdev-hw-api.vercel.app/api/v1/aleks-zasimov/comments', {
-  method: 'GET',
-});
-fetchPromise.then((response) => {
-  // Запускаем, преобразовываем сырые данные от API в JSON-формат
-  const jsonPromise = response.json();
-
-  // Подписываемся на результат преобразования
-  jsonPromise.then((responseData) => {
-    appComments = responseData.comments.map((comment) => {
-      return {
-        //Имя автора
-        name: comment.author.name,
-        //Преобразуем дату строку в date
-        dates: new Date(comment.date),
-        //Текст комментария
-        text: comment.text,
-        //Количество лайков
-        like: comment.likes,
-        //Лайки
-        Iliked: false,
-      }
-    })
-
-    comments = appComments;
-    renderComments();
   });
-});
-//Рендер комментариев
-function renderComments() {
-  const commentHtmlResult = appComments.map((comment, id) => {
-    let Iliked = '';
-    let dates = '';
+}
 
-    if (comment.Iliked) {
-      Iliked = '-active-like';
-    }
+getApi();
 
-    if (comment.date) {
-      dates = comment.date;
-    } else {
-      const date = new Date();
-      const day = ("0" + date.getDate()).slice(-2);
-      const month = ("0" + (date.getMonth() + 1)).slice(-2);
-      const year = date.getFullYear().toString().slice(-2);
-      const hours = ("0" + date.getHours()).slice(-2);
-      const minutes = ("0" + date.getMinutes()).slice(-2);
-      dates = `${day}.${month}.${year} ${hours}:${minutes}`;
-    }
+const addLike = (e) => {
+  const comment = userComments[e.target.dataset.id];
+  comment.likes++;
+  comment.Iliked = true;
+}
 
-    return `<li class="comment" data-id="${id}">
-<div class="comment-header">
-<div class="comment-name">${comment.name}</div>
-<div>${dates}</div>
-</div>
-<div class="comment-body">
-<div class="comment-text">${comment.text}</div>
-</div>
-<div class="comment-footer">
-<div class="likes">
-<span class="likes-counter">${comment.like}</span>
-<button class="like-button ${Iliked}" data-id="${id}"></button>
-</div>
-</div>
-</li>`;
-  }).join("");
+const delLike = (e) => {
+  const comment = userComments[e.target.dataset.id];
+  comment.likes--;
+  comment.Iliked = false;
+}
 
-  commentList.innerHTML = commentHtmlResult;
+const initLikeClick = () => {
+  const likeClickElems = document.querySelectorAll('.likes');
+  for (likeClickElem of likeClickElems) {
+    likeClickElem.addEventListener('click', (e) => {
+      e.stopPropagation();
+      (userComments[e.target.dataset.id].Iliked) ? delLike(e) : addLike(e);
+      renderComments();
+    })
+  }
+}
+
+// Первый вариант ответа на коммент
+const answerComment = () => {
+  const boxCommentsTexts = document.querySelectorAll('.comment');
+  boxCommentsTexts.forEach((comment) => {
+    comment.addEventListener('click', (e) => {
+      const author = comment.querySelector('.comment-header div:first-child').textContent;
+      const text = comment.querySelector('.comment-text').textContent;
+      textAreaComment.value = `@${author} \n\n > ${text}, `;
+    });
+  });
+}
+answerComment();
+
+const renderComments = () => {
+  const commentHtml = userComments.map((comment,index) => {
+    (comment.Iliked) ? Iliked = '-active-like' : Iliked = '';
+    return `<li class="comment">
+      <div class="comment-header">
+        <div class="comment-user-name">${comment.name}</div>
+        <div class="comment-date">${comment.date}</div>
+      </div>
+      <div class="comment-body">
+        <div class="comment-text">
+          ${comment.text}
+        </div>
+      </div>
+      <div class="comment-footer">
+        <div class="likes">
+          <span class="likes-counter">${comment.likes}</span>
+          <button class="like-button ${Iliked}" data-id='${index}'></button>
+        </div>
+      </div>
+    </li>`;
+  }).join('');
+
+  boxComments.innerHTML = commentHtml;
   initLikeClick();
-  addCommentListener();
+  answerComment();
 }
-//Функция, которая отправляет новый добавленный комментарий на сервер по указанному API
-const sendCommentToServer = (comment) => {
-  fetch('https://webdev-hw-api.vercel.app/api/v1/aleks-zasimov/comments', {
-    method: 'POST',
-    body: JSON.stringify(comment),
-  })
-    .then((response) => response.json())
-    .then((responseData) => {
-      // Обработка ответа сервера (при необходимости)
-      console.log('Комментарий успешно отправлен на сервер:', responseData);
-    })
-    .catch((error) => {
-      // Обработка ошибок при отправке на сервер
-      console.error('Ошибка при отправке комментария на сервер:', error);
-    });
-};
-//Добавление комментария
-const addComment = () => {
-  const date = new Date();
 
-  if (validate()) {
-    const day = ("0" + date.getDate()).slice(-2);
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    const year = date.getFullYear().toString().slice(-2);
-    const hours = ("0" + date.getHours()).slice(-2);
-    const minutes = ("0" + date.getMinutes()).slice(-2);
-    const formattedDate = `${day}.${month}.${year} ${hours}:${minutes}`;
-
-    const newComment = {
-      name: userName.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
-      date: formattedDate,
-      text: textComment.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
-      like: 0,
-    };
-
-    appComments.push(newComment);
-    sendCommentToServer(newComment); // Отправка комментария на сервер
-
-
-    renderComments();
-    textComment.value = "";
-    userName.value = "";
-    button.setAttribute('disabled', '');
-  }
-};
-//Проверка форм на введенные данные
-const validate = () => {
-  const userNameValue = userName.value.trim();
-  const textCommentValue = textComment.value.trim();
-
-  const isUserNameValid = userNameValue.length >= 3;
-  const isTextCommentValid = textCommentValue.length >= 3;
-
-  userName.classList.toggle('error', !isUserNameValid);
-  textComment.classList.toggle('error', !isTextCommentValid);
-
-  if (!isUserNameValid) {
-    if (userNameValue === "Напишите не менее 3 символов") {
-      userName.placeholder = "Напишите не менее 3 символов";
-    } else {
-      userName.value = "";
-      userName.placeholder = "Напишите не менее 3 символов";
-      console.log("Напишите не менее 3 символов (Имя)");
-    }
-  }
-
-  if (!isTextCommentValid) {
-    if (textCommentValue === "Напишите не менее 3 символов") {
-      textComment.placeholder = "Напишите не менее 3 символов";
-    } else {
-      textComment.value = "";
-      textComment.placeholder = "Напишите не менее 3 символов";
-      console.log("Напишите не менее 3 символов (Текст комментария)");
-    }
-  }
-
-  return isUserNameValid && isTextCommentValid;
-};
-//Слушаем кнопку и добавляем комментарий
-button.addEventListener('click', (event) => {
-  addComment();
-});
-//Слушаем ввод в поле и добавляем комментарий
-addForm.addEventListener('input', (event) => {
-  if (validate) {
-    button.removeAttribute('disabled');
-    button.classList.remove('add-form-button-disabled');
-  }
-});
-//Слушаем нажатие Enter
-addForm.addEventListener('keydown', (event) => {
-  if (event.ctrlKey && event.key === "Enter") {
-    event.preventDefault();
-    addComment();
-  }
-});
 renderComments();
-// Функция добавляет ответ на комментарий
-function addCommentListener() {
-  const comments = document.querySelectorAll('.comment');
 
-  comments.forEach((comment) => {
-    comment.addEventListener('click', () => {
-      const answer = comment.querySelector('.comment-body').textContent;
-      const nameUser = comment.querySelector('.comment-name').textContent;
-      textComment.value = `>${answer}${nameUser}.,`;
+const addComment = () => {
+  const fetchPromise = fetch("https://webdev-hw-api.vercel.app/api/v1/alex-zasimov/comments", {
+    method: "POST",
+    body: JSON.stringify({
+      id: 1,
+      date: `${now.toLocaleString().slice(0,-3)}`,
+      likes: 0,
+      isLiked: false,
+      text: `${textAreaComment.value
+      .replaceAll('<', '&lt;')
+      .replaceAll('<', '&gt;')}`,
+      name: inputName.value
+      .replaceAll('<', '&lt;')
+      .replaceAll('<', '&gt;'), 
+      })
+    });
+  
+  fetchPromise.then((response) => {
+    const jsonPromise = response.json();
+
+    // Подписываемся на результат преобразования
+    jsonPromise.then((responseData) => {
+      // получили данные и рендерим их в приложении
+      userComments = responseData.comments;
     });
   });
+
+  getApi();
+  renderComments();
+  answerComment();
+  inputName.value = '';
+  textAreaComment.value = '';
 }
-addCommentListener();
+
+buttonNewComment.addEventListener('click', function () {
+  let oldComments = boxComments.innerHTML;
+
+  if (inputName.value === '') {
+    inputName.classList.add('error');
+    return;
+  } if (textAreaComment.value === '') {
+    textAreaComment.classList.add('error');
+    return;
+  } else {
+    getApi();
+    addComment();
+    inputName.classList.remove('error');
+    textAreaComment.classList.remove('error');
+    }
+});
