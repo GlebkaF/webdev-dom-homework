7af55d2ee1c3d0774212cd0c54af34ddf7c2cbf4
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-07-15 20:13:59 +0400

    выносит список и форму  в отдельный компонент

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import { addComment, getComments } from "./api.js";
 import { getCommentDate } from "./components/date-component.js";
+import { renderListComponent } from "./components/list-and-add-component.js";
 import { renderLoginComponent } from "./components/login-component.js";
 
 
@@ -106,37 +107,7 @@ const renderApp = () =>{
     return;
   }
 
-  let appHtml = `
-        <ul class="comments" id="comment-list">
-          ${usersHtml}
-          </ul>
-          <div class="add-form-row" style="justify-content: center" >
-            <button class="add-form-button" id="delete-button">Удалить комментарий</button>
-          </div>
-      <div id="add">
-        <div class="add-form">
-          <input
-            id="name-input"
-            type="text"
-            class="add-form-name"
-            placeholder="Введите ваше имя"
-          />
-          <textarea
-            id="comment-text"
-            type="textarea"
-            class="add-form-text"
-            placeholder="Введите ваш коментарий"
-            rows="4"
-          ></textarea>
-          <div class="add-form-row">
-            <button class="add-form-button" id="write-button">Написать</button>
-          </div>
-        </div>
-      </div>
-      <div id="load" class="hidden">
-        <h3 style="font-family: Helvetica; color: #ffffff;">Комментарий добавляется...</h3>
-      </div>`
-    appEl.innerHTML = appHtml;
+  renderListComponent({ usersHtml, appEl });
 
 
   const writeButtonEl = document.getElementById("write-button");
