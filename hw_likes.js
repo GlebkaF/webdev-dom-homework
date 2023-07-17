f9dc4bbd1c88cd957943330bf5b7dea38a5d66d7
Arvik1982 <phone.kulikovarseniy@gmail.com>
2023-07-17 15:56:08 +0300

    дз

---
+++ hw_likes.js
@@ -0,0 +1,54 @@
+
+const comments = [
+    {
+    name: `Глеб Фокин`,
+    date: `12.02.22 12:18`,
+    comment: `first comment on page`,
+    like: 3,
+    isLiked: true
+  },
+    {
+    name: `Варвара Н.`,
+    date:`13.02.22 12:18`,
+    comment: `Мне нравится как оформлена страница`,
+    like: 75,
+    isLiked: true
+  }
+];
+
+
+
+
+// for (const i of comments) {
+    
+    
+//     let a =i.like
+//  if(i.isLiked===false)   {
+//     i.isLiked=true
+//     console.log(a+1)
+//     console.log(i.isLiked)}
+//  else if(i.isLiked===true){
+//     i.isLiked=false
+//     console.log(a-1)}
+// }
+//  console.log(comments)
+
+
+ comments.map(function(element){
+
+
+    let a =element.like
+    if(element.isLiked===false)   {
+       element.isLiked=true
+       element.like=a+1
+       console.log(a+1)
+       console.log(element.isLiked)}
+    else if(element.isLiked===true){
+       element.isLiked=false
+       element.like=a-1
+       console.log(a-1)}
+
+       return comments
+ })
+
+
