08415e87b87b0435f29ea80fcb4ab69485763642
Maady <vlad030911@gmail.com>
2023-07-22 16:23:29 +0300

    gg

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const btnElement = document.getElementById("btnId");
 const inputName = document.getElementById("nameTextId");
 const inputText = document.getElementById("commentTextId");
 const likeElement = document.getElementsByClassName("like-button");
-
+const formBg = document.querySelector('.add-form'); 
+let textAnswerHtml = "";
+let indexOld = 0;
 // массив людей оставивших комменты
+
+log(likeElement);
+
+
 
 const commentators = [
     {
@@ -16,15 +22,25 @@ const commentators = [
         data: '12.02.22 12:18',
         textComment: "Это будет первый комментарий на этой странице",
         likeQuantity: 3,
-        LikeActive: false
+        LikeActive: false,
+        isEdit: false,
+        isQuote: false,
+        animationClass: "",
+        isAnswers: "",
+        isReduction: false
         
     },
     {
         name: 'Варвара Н.',
-        data: '13.02.2022 19:22',
+        data: '13.02.22 19:22',
         textComment: "Мне нравится как оформлена эта страница! ❤",
         likeQuantity: 75,
-        LikeActive: true
+        LikeActive: true,
+        isEdit: false,
+        isQuote: false,
+        animationClass: "",
+        isAnswers: "",
+        isReduction: false
     }
 ]
 
@@ -55,7 +71,7 @@ const btnErrAdd = () => {
 
 const getDelCard = (element) => {
     setTimeout(() => {
-        element.parentElement.classList.add('del-card');
+        element.closest('.like-button').classList.add('del-card');
     }, 300)
     element.classList.remove('del');
     element.classList.add('exet-del');
@@ -64,11 +80,12 @@ const getDelCard = (element) => {
 const commentDel = () => {
     const btnFormElement = document.querySelectorAll(".del");
     btnFormElement.forEach((element) => {
-        element.addEventListener('click', () => {
+        element.addEventListener('click', (e) => {
+            e.stopPropagation();
             getDelCard(element);
             setTimeout(() => {
                 const indexElement = element.dataset.index;
-                commentators.splice(+indexElement, 1);
+                commentators.splice(indexElement, 1);
                 renderComments()
             },800)
 
@@ -76,10 +93,26 @@ const commentDel = () => {
     })
 }
 
+const answComment = () => {
+    const comments = document.querySelectorAll('.comment-text');
+    // const btnElement = document.getElementById("btnId"); 
+    comments.forEach((comment,index) => {
+        comment.addEventListener('click', () => {
+            formBg.classList.add('comment-new-bg');
+            const commentator = commentators[index]
+            const oldComment = commentator.textComment
+            inputText.placeholder = `Введите Ответ Пользователю ${commentator.name}`;
+            textAnswerHtml = oldComment;    
+            indexOld = index;
+        })
+    });
+} 
+
 // Функция добавления лайка
 function addLike () {
     Array.from(likeElement).forEach((element,index) => {
-        element.addEventListener('click', () => {
+        element.addEventListener('click', (e) => {
+            e.stopPropagation();
             const commentator = commentators[index];
             if (commentator.LikeActive === true) {
                 commentator.LikeActive = false;
@@ -99,26 +132,30 @@ function addLike () {
 // Функция редактирования комментария
 const clickEventEditComment = () => {
     const redirectElements = document.querySelectorAll(".red");
-    const textElements = document.querySelectorAll(".comment-text");
-    redirectElements.forEach((redirectElement,indexDel) => {
-        redirectElement.addEventListener('click', () => {
-            let text;
-            if (redirectElement.innerText === "Редактировать") {
-                redirectElement.innerHTML = "Ок"
-                
-                // const parent = textElements[indexDel].innerText
-                textElements[indexDel].innerHTML = `<textarea type="textarea"  class="add-form-text" placeholder="Введите ваш коментарий" rows="4"></textarea>`;
-                
-    
-            } else { 
-                redirectElement.innerHTML = "Редактировать"
-                commentators[indexDel].textComment = '';
-                commentators[indexDel].textComment = textElements[indexDel].value;
-                renderComments();
+    redirectElements.forEach((redirectElement, indexEl) => {
+        redirectElement.addEventListener('click', (e) => {
+            e.stopPropagation();
+            log(redirectElement.innerHTML); 
+            const index = redirectElement.dataset.index;
+            const comment =  commentators[index];
+            log(comment);
+            if (comment.isEdit) {
+                const edit = document.querySelector('.add-edit');
+                log(edit);
+                comment.textComment = edit.value;
+                if (comment.textComment.length === 0) {
+                    commentators.splice(index,1);
+                }
+                comment.isEdit = false;
+            } else {
+                comment.isEdit = true;
             }
+            renderComments();
         })
     })
 }
+
+
 
 
 // Рендер
@@ -129,7 +166,10 @@ const getLikeClass = (element) => {
 
 const renderComments = () => {
 
+    
+
     const commentatorsHtml = commentators.map((commentator, index) => {
+        const edit = commentator.isEdit;
         return `<li class="comment ${commentator.animationClass}">
         <i class='bx bx-x del' data-index="${index}"></i>
       <div class="comment-header">
@@ -137,12 +177,11 @@ const renderComments = () => {
         <div>${commentator.data}</div>
       </div>
       <div class="comment-body">
-        <div class="comment-text">
-          ${commentator.textComment}
-        </div>
-      </div>
+            ${commentator.isAnswers.length !== 0 ? `<div class="c">${commentator.isAnswers}</div>`: ""}
+          ${edit ? `<textarea type="textarea" class="add-form-text add-edit" placeholder="Введите ваш коментарий" rows="4">${commentator.textComment}</textarea>` : `<div class="comment-text">${commentator.textComment}</div>`}
+   
       <div class="comment-footer comment-footer_new">
-       <button  class="add-form-button red">Редактировать</button>
+        ${commentator.isReduction ? `<button  class="add-form-button red" data-index="${index}">Редактировать</button>` : ''}
         <div class="likes">
           <span class="likes-counter">${commentator.likeQuantity}</span>
           <button class="${getLikeClass(commentator.LikeActive)}"></button>
@@ -151,14 +190,21 @@ const renderComments = () => {
     </li>`;
     }).join("");
     cardElements.innerHTML = commentatorsHtml;
-    // red()
     commentDel();
     addLike();
     clickEventEditComment();
+    answComment();
 }
 
 renderComments();
 
+const eventErrors = (element) => {
+    return element
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+}
 
 // Функция добавления нового комментария
 
@@ -175,7 +221,7 @@ const clickEventAddComment = () => {
 
     if (inputName.value.length === 0) {
         inputName.classList.add("error");
-        btnErrAdd()
+        btnErrAdd();
         return;
     }
 
@@ -185,20 +231,29 @@ const clickEventAddComment = () => {
         return;
     }
 
+    formBg.classList.remove('comment-new-bg');
+    inputText.placeholder = 'Введите ваш коментарий'
+    const commentText = document.querySelectorAll('.comment-text');
+                const isAnswerTest =  commentText[indexOld].innerText === textAnswerHtml;
+    const test = () => isAnswerTest ? textAnswerHtml : "";
+
     commentators.push(
         {
-            name: inputName.value,
+            name: eventErrors(inputName.value),
+            textComment: eventErrors(inputText.value),
             data: addDate(),
             likeQuantity: 0,
-            textComment: inputText.value,
-            animationClass: "comment_animation"
-        }
+            animationClass: "comment_animation",
+            isQuote: false,
+            isAnswers: test(),
+            isReduction: true
+            }
     )
-
     renderComments();
     commentators[commentators.length - 1].animationClass = "";
     document.getElementById("nameTextId").value = '';
     document.getElementById("commentTextId").value = '';
+    textAnswerHtml = ""
 }
 
 
@@ -213,4 +268,5 @@ document.addEventListener('keyup', (key) => {
 
 btnElement.addEventListener( 'click', () => clickEventAddComment())
 log("It works!");
+
 
