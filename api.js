c47961823b583be0903b904eff14f60270747589
alexkm59 <alexkm59@yandex.ru>
2023-07-24 11:20:05 +0500

    hw2_16

--- api.js
+++ api.js
@@ -1,14 +1,22 @@
 
 
 // Функция GET для API
+const host = "https://wedev-api.sky.pro/api/v1/aleks-karmanov/comments";
+const host2 = "https://wedev-api.sky.pro/api/v2/:aleks-karmanov/comments";
+
+export let token;
+
+export const setToken = (newToken) => {
+    token = newToken;
+};
 
 export const getComments = () => {
 
-   return fetch("https://wedev-api.sky.pro/api/v1/aleks-karmanov/comments", {
+    return fetch(host2, {
         method: "GET",
     })
         .then((response) => {
-            
+
             if (response.status === 500) {
                 // Код обработки ошибки
                 throw response.status;
@@ -17,8 +25,8 @@ export const getComments = () => {
                 return response.json();
             }
         })
-        
-        
+
+
         .catch((error) => {
             if (error === 500) {
                 alert("Сервер сломался, попробуй позже");
@@ -31,61 +39,90 @@ export const getComments = () => {
 
 };
 
-export const postComment = ({fetchAndRenderComments}) => {
-    
+export const postComment = ({ fetchAndRenderComments }) => {
+
     const nameElement = document.querySelector('.add-form-name');
     const textElement = document.querySelector('.add-form-text');
     const commentLoadElement = document.querySelector('.comment-loader');
     const addFormElement = document.querySelector('.add-form');
-    
+
     addFormElement.classList.add('disableLoader');
     commentLoadElement.classList.remove('disableLoader');
 
-return fetch("https://wedev-api.sky.pro/api/v1/aleks-karmanov/comments", {
-    method: "POST",
-    body: JSON.stringify({
-        text: textElement.value,
-        name: nameElement.value,
-        // forceError: false,
+    return fetch(host2, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            text: textElement.value,
+            name: nameElement.value,
+            // forceError: false,
+        })
     })
-})
-    .then((response) => {
-        if ((response.status === 400) || (response.status === 500)) {
-            // Код обработки ошибки
-            throw response.status;
+        .then((response) => {
+            if ((response.status === 400) || (response.status === 500)) {
+                // Код обработки ошибки
+                throw response.status;
 
-        }
-        else {
-            // return response.json();
+            }
+            else {
+                // return response.json();
+                commentLoadElement.classList.add('disableLoader');
+            }
+
+        })
+        .then(() => {
+
+            addFormElement.classList.remove('disableLoader');
+
+        })
+        .then(() => {
+
+            fetchAndRenderComments();
+            // Очистка формы
+            nameElement.value = "";
+            textElement.value = "";
+        })
+        .catch((error) => {
+            if (error === 400) {
+                alert("Имя или текст содержат менее 3-х символов");
+            }
+            if (error === 500) {
+                alert("Сервер сломался, попробуй позже");
+            }
+            else {
+                alert("Кажется, у вас сломался интернет, попробуйте позже");
+            }
+            addFormElement.classList.remove('disableLoader');
             commentLoadElement.classList.add('disableLoader');
-        }
-
-    })
-    .then(() => {
-
-        addFormElement.classList.remove('disableLoader');
-
-    })
-    .then(() => {
-
-        fetchAndRenderComments();
-        // Очистка формы
-        nameElement.value = "";
-        textElement.value = "";
-    })
-    .catch((error) => {
-        if (error === 400) {
-            alert("Имя или текст содержат менее 3-х символов");
-        }
-        if (error === 500) {
-            alert("Сервер сломался, попробуй позже");
-        }
-        else {
-            alert("Кажется, у вас сломался интернет, попробуйте позже");
-        }
-        addFormElement.classList.remove('disableLoader');
-        commentLoadElement.classList.add('disableLoader');
-        // TODO: Отправлять в систему сбора ошибок
-        console.warn(error);
-    })
+            // TODO: Отправлять в систему сбора ошибок
+            console.warn(error);
+        })
 };
+
+
+export function login({ login, password }) {
+    const addFormCommentElement = document.querySelector('.add-form-comment');
+    return fetch("https://wedev-api.sky.pro/api/user/login", {
+        method: "POST",
+        body: JSON.stringify({
+            login,
+            password,
+        }),
+    }).then((response) => {
+        // Проверка авторицазии
+        if (response.status === 201) {
+            addFormCommentElement.classList.remove('disableLoader');
+            console.log(response);
+        };
+        if (response.status === 400) {
+            alert("Введен не верный логин или пароль, попробуйте еще раз.");
+        };
+
+        return response.json();
+    })
+        .catch(() => {
+            alert("Какая-то ошибка");
+        })
+}
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { dataFormat } from "./formatDate.js";
-import { getComments, postComment } from "./api.js";
-import {renderComments} from "./renderComments.js";
+import { getComments, postComment, login, token, setToken } from "./api.js";
+import { renderComments } from "./renderComments.js";
 
 
 const buttonElement = document.querySelector('.add-form-button');
@@ -10,7 +10,6 @@ const hederLoadElement = document.querySelector('.loader');
 let comments = [];
 
 // Функция GET для API
-
 const fetchAndRenderComments = () => {
     getComments()
         .then((responseData) => {
@@ -48,15 +47,77 @@ const disableButton = () => {
         buttonElement.disabled = true;
     }
 };
-
 nameElement.addEventListener("input", disableButton);
 textElement.addEventListener("input", disableButton);
 
+const loginElement = document.getElementById("login");
+
+const loginHtml = `<a class="login-link" href="#" id="link-to-in">Чтобы добавить комментарий, авторизуйтесь </a>`;
+
+loginElement.innerHTML = loginHtml;
+
+
+
+const loginAutoriz = () => {
+    let loginLinkButton = document.getElementById("link-to-in");
+    loginLinkButton.addEventListener('click', () => {
+        const autorizationFormHtml = `
+        <div class="add-form"> Форма входа
+            <br>
+            <input id="login-input" type="text" class="add-form-name add-login" placeholder="Логин" />
+            <br>
+            <input id="pass-input" type="text" class="add-form-name add-pass" placeholder="Пароль" />
+            <button id="add-form-login" class="add-form-button">Войти</button>
+            <a class="login-link" href="#" id="link-to-login">Зарегистрироваться</a>
+        </div>`;
+
+        loginElement.innerHTML = autorizationFormHtml;
+
+        const loginButton = document.getElementById("add-form-login");
+        const loginInputElement = document.getElementById("login-input");
+        const passwordInputElement = document.getElementById("pass-input");
+
+        loginButton.addEventListener('click', () => {
+            login({
+                login: loginInputElement.value,
+                password: passwordInputElement.value,
+            }).then((responseData) => {
+                setToken(responseData.user.token);
+                nameElement.value = responseData.user.name;
+                loginElement.innerHTML = ` `;
+            });
+
+        });
+
+
+        const registrationLinkButton = document.getElementById("link-to-login");
+        registrationLinkButton.addEventListener('click', () => {
+
+            const registrationFormHtml = `
+                <div class="add-form"> Форма регистрации
+                    <input type="text" class="add-form-name add-login" placeholder="Имя" />
+                    <br>
+                    <input type="text" class="add-form-name add-login" placeholder="Логин" />
+                    <br>
+                    <input type="text" class="add-form-name add-pass" placeholder="Пароль" />
+                    <button class="add-form-button">Зарегистрироваться</button>
+                    <a class="login-link" href="#" id="link-to-in">Войти</a>
+                </div>`;
+
+            loginElement.innerHTML = registrationFormHtml;
+            loginAutoriz();
+        });
+
+    });
+
+};
+
+loginAutoriz();
 
 // Форма добавления нового комментария в ленту
 buttonElement.addEventListener('click', () => {
 
-    postComment({fetchAndRenderComments});
-    
+    postComment({ fetchAndRenderComments });
+
 });
 
