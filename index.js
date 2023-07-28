aa06f8b1cdaaf999f428265dc2aee5de7656638c
ErmushinRomanWeb <ermushinr@gmail.com>
2023-07-29 02:36:23 +0300

    Start

--- index.js
+++ index.js
@@ -1,133 +1,178 @@
 "use strict";
-    const formButtonElement = document.getElementById("form-button");
-    const commentBlockElement = document.getElementById("comment-block");
-    const nameInputElement = document.getElementById("name-input");
-    const textInputElement = document.getElementById("text-input");
+const formButtonElement = document.getElementById("form-button");
+const commentBlockElement = document.getElementById("comment-block");
+const nameInputElement = document.getElementById("name-input");
+const textInputElement = document.getElementById("text-input");
 
-    const commentators = [
-        {
-            name: 'Глеб Фокин',
-            commentText: 'Это будет первый комментарий на этой странице',
-            time: '12.02.22 12:18',
-            likes: 3,
-            isLiked: false,
-        },
-        {
-            name: 'Варвара Н.',
-            commentText: 'Мне нравится как оформлена эта страница! ❤',
-            time: '13.02.22 19:22',
-            likes: 3,
-            isLiked: false,
-        }
-    ];
-
-    const likeMaker = () => {
-        const likeButtonElements = document.querySelectorAll('.like-button');
-        likeButtonElements.forEach((likeButtonElement) => {
-            likeButtonElement.addEventListener('click', (event) => {
-                event.stopPropagation()
-                const index = likeButtonElement.dataset.index
-                const like = commentators[index];
-                if (like.isLiked === false) {
-                    like.isLiked = true;
-                    like.likes += 1;
-                    renderCommentators()
-                } else {
-                    like.isLiked = false;
-                    like.likes -= 1;
-                    renderCommentators()
-                }
-            })
-        })
-    }
-    
-    const replyТoСomment = () => {
-      const commentElements = document.querySelectorAll('.comment');
-      commentElements.forEach((commentElement) => {
-        commentElement.addEventListener('click', (event) => {
-          const index = commentElement.dataset.index;
-          console.log(index);
-          const person = commentators[index]
-          textInputElement.value = `> ${person.commentText} \n \n  ${person.name},`;
-        })
-      })
-    }
-
-    const newlikeColor = (element) => {
-        if (element) {
-            return 'like-button -active-like';
-        } else {
-            return 'like-button';
-        }
-        // return element ? 'like-button -active-like' : 'like-button'; 
+fetch('https://wedev-api.sky.pro/api/v1/:ErmushinRoman/comments',
+  {
+   method: 'GET', 
+}).then((response) => {
+  response.json().then((responseData) => {
+    const appComments = responseData.comments.map((comment) => {
+      return {
+        name: comment.author.name,
+        time: new Date(comment.date),
+        commentText: comment.text,
+        likes: comment.likes,
+        isLiked: false,
       }
-    
-    const renderCommentators = () => {
-        const commentatorsHtml = commentators.map((commentator, index) => {
-            return `<li data-index="${index}" class="comment">
-            <div class="comment-header">
-              <div>${commentator.name}</div>
-              <div>${commentator.time}</div>
-            </div>
-            <div class="comment-body">
-              <div class="comment-text">
-                ${commentator.commentText}
-              </div>
-            </div>
-            <div class="comment-footer">
-              <div class="likes">
-                <span class="likes-counter">${commentator.likes}</span>
-                <button data-index="${index}" class="${newlikeColor(commentator.isLiked)}"></button>
-              </div>
-            </div>`
-        }).join('');
-        commentBlockElement.innerHTML = commentatorsHtml;
-        likeMaker();
-        replyТoСomment();
-    }
-
+    });
+    commentators = appComments;
+    console.log(commentators);
     renderCommentators()
+  })
+})
 
-    const dateInComment = () => {
-      const myDate = new Date();
-      let day = myDate.getDate();
-      if (day < 10) {
-        day = "0" + day;
-      }
-      let month = myDate.getMonth() + 1;
-      if (month < 10) {
-        month = "0" + month;
-      }
-      let year = myDate.getFullYear() % 100;
-      let hours = myDate.getHours();
-      if (hours < 10) {
-        hours = "0" + hours;
-      }
-      let minutes = myDate.getMinutes();
-      if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-      let timeWithoutSeconds = hours + ":" + minutes;
-      const fullDate =
-        day + "." + month + "." + year + " " + timeWithoutSeconds;
-      return fullDate;
-    };
+const dateInAPI = (dateInAPI) => {
+  const myDate = dateInAPI;
+  let day = myDate.getDate();
+  if (day < 10) {
+    day = "0" + day;
+  }
+  let month = myDate.getMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
+  let year = myDate.getFullYear() % 100;
+  let hours = myDate.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  let minutes = myDate.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  let timeWithoutSeconds = hours + ":" + minutes;
+  const fullDate =
+    day + "." + month + "." + year + " " + timeWithoutSeconds;
+  return fullDate;
+};
+
+let commentators = [
+    // {
+    //     name: 'Глеб Фокин',
+    //     commentText: 'Это будет первый комментарий на этой странице',
+    //     time: '12.02.22 12:18',
+    //     likes: 3,
+    //     isLiked: false,
+    // },
+    // {
+    //     name: 'Варвара Н.',
+    //     commentText: 'Мне нравится как оформлена эта страница! ❤',
+    //     time: '13.02.22 19:22',
+    //     likes: 3,
+    //     isLiked: false,
+    // }
+];
+
+const likeMaker = () => {
+    const likeButtonElements = document.querySelectorAll('.like-button');
+    likeButtonElements.forEach((likeButtonElement) => {
+        likeButtonElement.addEventListener('click', (event) => {
+            event.stopPropagation()
+            const index = likeButtonElement.dataset.index
+            const like = commentators[index];
+            if (like.isLiked === false) {
+                like.isLiked = true;
+                like.likes += 1;
+                renderCommentators()
+            } else {
+                like.isLiked = false;
+                like.likes -= 1;
+                renderCommentators()
+            }
+        })
+    })
+}
+    
+const replyТoСomment = () => {
+  const commentElements = document.querySelectorAll('.comment');
+  commentElements.forEach((commentElement) => {
+    commentElement.addEventListener('click', (event) => {
+      const index = commentElement.dataset.index;
+      console.log(index);
+      const person = commentators[index]
+      textInputElement.value = `> ${person.commentText} \n \n  ${person.name},`;
+    })
+  })
+}
+
+const newlikeColor = (element) => {
+    if (element) {
+        return 'like-button -active-like';
+    } else {
+        return 'like-button';
+    }
+    // return element ? 'like-button -active-like' : 'like-button'; 
+  }
+
+const renderCommentators = () => {
+    const commentatorsHtml = commentators.map((commentator, index) => {
+        return `<li data-index="${index}" class="comment">
+        <div class="comment-header">
+          <div>${commentator.name}</div>
+          <div>${dateInAPI(commentator.time)}</div>
+        </div>
+        <div class="comment-body">
+          <div class="comment-text">
+            ${commentator.commentText}
+          </div>
+        </div>
+        <div class="comment-footer">
+          <div class="likes">
+            <span class="likes-counter">${commentator.likes}</span>
+            <button data-index="${index}" class="${newlikeColor(commentator.isLiked)}"></button>
+          </div>
+        </div>`
+    }).join('');
+    commentBlockElement.innerHTML = commentatorsHtml;
+    likeMaker();
+    replyТoСomment();
+}
+
+renderCommentators()
+
+const dateInComment = () => {
+  const myDate = new Date();
+  let day = myDate.getDate();
+  if (day < 10) {
+    day = "0" + day;
+  }
+  let month = myDate.getMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
+  let year = myDate.getFullYear() % 100;
+  let hours = myDate.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  let minutes = myDate.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  let timeWithoutSeconds = hours + ":" + minutes;
+  const fullDate =
+    day + "." + month + "." + year + " " + timeWithoutSeconds;
+  return fullDate;
+};
 
 
 
 
 
-    formButtonElement.addEventListener("click", () => {
-      nameInputElement.classList.remove("error");
-      if (nameInputElement.value === "") {
-        nameInputElement.classList.add("error");
-        return;
-      }
-      textInputElement.classList.remove("error");
-      if (textInputElement.value === "") {
-        textInputElement.classList.add("error");
-        return;
-      }
+formButtonElement.addEventListener("click", () => {
+  nameInputElement.classList.remove("error");
+  if (nameInputElement.value === "") {
+    nameInputElement.classList.add("error");
+    return;
+  }
+  textInputElement.classList.remove("error");
+  if (textInputElement.value === "") {
+    textInputElement.classList.add("error");
+    return;
+  }
 
       commentators.push({
         name: nameInputElement.value,
