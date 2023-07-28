ffc4eeb689336da6a93c7fa91403a92902711c8e
Zaporozhtsev Pavel <webzap@proton.me>
2023-07-29 00:45:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,162 +5,195 @@ const inputText = document.getElementById("add-form-text");
 const buttonSend = document.querySelector(".add-form-button");
 const listComments = document.querySelector(".comments");
 
-let PEOPLE = [
-  {
-    name: "Глеб Фокин",
-    comment: "Это будет первый комментарий на этой странице",
-    countLikes: 3,
-    isLiked: false,
-    isItDate: "12.02.22 12:18",
-  },
-  {
-    name: "Варвара Н.",
-    comment: "Мне нравится как оформлена эта страница! ❤",
-    countLikes: 76,
-    isLiked: true,
-    isItDate: "13.02.22 19:22",
-  },
-];
+let PEOPLE = [];
 
-const addDate = () => {
-  let nowDate = new Date();
-  let time = {
-    hour: "numeric",
-    minute: "numeric",
-  };
-  let date = {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  };
+const appPromise = () => {
+    return fetch("https://wedev-api.sky.pro/api/v1/:zaporozhtsev/comments", {
+        method: "GET",
+    }).then((response) => {
+        response.json().then((responseData) => {
+            PEOPLE = responseData.comments;
+            renderPeople();
+        });
+    });
+};
 
-  return (
-    nowDate.toLocaleDateString("ru", date) +
-    " " +
-    nowDate.toLocaleTimeString("ru", time)
-  );
+// const deletePromise = (id) => {
+//     return fetch(
+//         "https://wedev-api.sky.pro/api/v1/:zaporozhtsev-pavely/comments/" + id,
+//         {
+//             method: "DELETE",
+//         }
+//     ).then((response) => {
+//         response.json().then((responseData) => {
+//             PEOPLE = responseData.comments;
+//             renderPeople();
+//         });
+//     });
+// };
+
+const promiseSend = () => {
+    return fetch("https://wedev-api.sky.pro/api/v1/:zaporozhtsev/comments", {
+        method: "POST",
+        body: JSON.stringify({
+            text: inputText.value,
+            name: inputName.value,
+        }),
+    }).then((responseData) => {
+        responseData.json().then((responseJson) => {
+            PEOPLE = responseJson.comments;
+            renderPeople();
+        });
+    });
+};
+
+const addDate = (value) => {
+    let nowDate = new Date(value);
+    let time = {
+        hour: "numeric",
+        minute: "numeric",
+    };
+    let date = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+    };
+
+    return (
+        nowDate.toLocaleDateString("ru", date) +
+        " " +
+        nowDate.toLocaleTimeString("ru", time)
+    );
 };
 
 function addComment() {
-  buttonSend.addEventListener("click", (event) => {
-    inputName.classList.remove("error");
-    inputText.classList.remove("error");
-    if (inputName.value.length === 0 && inputText.value.length === 0) {
-      inputName.classList.add("error");
-      return;
-    }
-    if (inputName.value.length === 0) {
-      inputName.classList.add("error");
-      return;
-    }
-    if (inputText.value.length === 0 && inputText.value.length === 0) {
-      inputText.classList.add("error");
-      return;
-    }
-    event.stopPropagation();
-    PEOPLE.push({
-      name: inputName.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
-      comment: inputText.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
-      isLiked: false,
-      countLikes: 0,
-      isItDate: addDate(),
+    buttonSend.addEventListener("click", (event) => {
+        inputName.classList.remove("error");
+        inputText.classList.remove("error");
+        if (inputName.value.length === 0 && inputText.value.length === 0) {
+            inputName.classList.add("error");
+            return;
+        }
+        if (inputName.value.length === 0) {
+            inputName.classList.add("error");
+            return;
+        }
+        if (inputText.value.length === 0 && inputText.value.length === 0) {
+            inputText.classList.add("error");
+            return;
+        }
+        event.stopPropagation();
+
+        // PEOPLE.push({
+        //     name: inputName.value
+        //         .replaceAll("<", "&lt;")
+        //         .replaceAll(">", "&gt;"),
+        //     comment: inputText.value
+        //         .replaceAll("<", "&lt;")
+        //         .replaceAll(">", "&gt;"),
+        //     isLiked: false,
+        //     countLikes: 0,
+        //     isItDate: addDate(),
+        // });
+
+        promiseSend();
+        appPromise();
+        renderPeople();
+        inputName.value = "";
+        inputText.value = "";
     });
-    renderPeople();
-    inputName.value = "";
-    inputText.value = "";
-  });
 }
 
-function addLike() {
-  const likeButtons = document.querySelectorAll(".like-button");
-  likeButtons.forEach((likeButtonEl) => {
-    likeButtonEl.addEventListener("click", () => {
-      const index = likeButtonEl.dataset.index;
-      let likeEl = PEOPLE[index];
-      if (likeEl.isLiked === false) {
-        PEOPLE[index].isLiked = true;
-        likeEl.countLikes++;
-        renderPeople();
-      } else {
-        PEOPLE[index].isLiked = false;
-        likeEl.countLikes--;
-        renderPeople();
-      }
+const addLike = () => {
+    const likeButtons = document.querySelectorAll(".like-button");
+    likeButtons.forEach((likeButtonEl, index) => {
+        likeButtonEl.addEventListener("click", () => {
+            let likeEl = PEOPLE[index];
+            console.log(likeEl);
+            if (likeEl.isLiked === false) {
+                likeEl.isLiked = true;
+                likeEl.likes++;
+                renderPeople();
+            } else {
+                likeEl.isLiked = false;
+                likeEl.likes--;
+                renderPeople();
+            }
+        });
     });
-  });
-}
+};
 
 const getLike = (elementPeople) => {
-  return elementPeople ? "like-button -active-like" : "like-button";
+    return elementPeople ? "like-button -active-like" : "like-button";
 };
 
 const takeCommentText = () => {
-  const comentText = document.querySelectorAll(".comment-text");
-  const textArea = document.getElementById("add-form-text");
-  comentText.forEach((textElement, index) => {
-    textElement.addEventListener("click", (event) => {
-      event.stopPropagation();
-      let textValue = textElement.textContent;
-      return (textArea.value = `>${textValue} ${PEOPLE[index].name}  Ответ - `);
+    const comentText = document.querySelectorAll(".comment-text");
+    const textArea = document.getElementById("add-form-text");
+    comentText.forEach((textElement, index) => {
+        textElement.addEventListener("click", (event) => {
+            event.stopPropagation();
+            let textValue = textElement.textContent;
+            return (textArea.value = `>${textValue} ${PEOPLE[index].name}  Ответ - `);
+        });
     });
-  });
 };
 
 const delCommentElement = () => {
-  const delColBut = document.querySelectorAll(".del-form-button");
-  delColBut.forEach((delButton, index) => {
-    delButton.addEventListener("click", (event) => {
-      event.stopPropagation();
-      PEOPLE.splice(index, 1);
-      renderPeople();
+    const delColBut = document.querySelectorAll(".del-form-button");
+    delColBut.forEach((delButton, id) => {
+        delButton.addEventListener("click", (event) => {
+            event.stopPropagation();
+
+            renderPeople();
+        });
     });
-  });
 };
 
 const checkError = () => {
-  buttonSend.addEventListener("click", () => {
-    inputName.classList.remove("error");
-    inputText.classList.remove("error");
-    if (inputName.value === "") {
-      inputName.classList.add("error");
-      return;
-    }
-    if (inputName.value === "") {
-      inputText.classList.add("error");
-      return;
-    }
-  });
+    buttonSend.addEventListener("click", () => {
+        inputName.classList.remove("error");
+        inputText.classList.remove("error");
+        if (inputName.value === "") {
+            inputName.classList.add("error");
+            return;
+        }
+        if (inputName.value === "") {
+            inputText.classList.add("error");
+            return;
+        }
+    });
 };
 
 const renderPeople = () => {
-  let render = PEOPLE.map((el, i) => {
-    return `<li class="comment">
+    let render = PEOPLE.map((el, i) => {
+        return `<li class="comment">
     <div class="comment-header">
-      <div>${el.name}</div>
-      <div>${el.isItDate}</div>
+      <div>${el.author.name}</div>
+      <div>${addDate(el.date)}</div>
     </div>
     <div class="comment-body">
       <div class="comment-text"  data-index="${i}">
-        ${el.comment}
+        ${el.text}
       </div>
     </div>
     <div class="comment-footer">
     <button class="del-form-button" data-index="${i}">Удалить</button>
       <div class="likes">
         <div>
-          <span class="likes-counter">${el.countLikes}</span>
-          <button class="${getLike(el.isLiked)}" data-index=${i}></button></div>
+          <span class="likes-counter">${el.likes}</span>
+          <button class="${getLike(el.isLiked)}" ></button></div>
       </div>
     </div>
   </li>`;
-  }).join("");
+    }).join("");
 
-  listComments.innerHTML = render;
-  addLike();
-  takeCommentText();
-  delCommentElement();
+    listComments.innerHTML = render;
+    addLike();
+
+    takeCommentText();
+    delCommentElement();
 };
-
-addLike();
+appPromise();
 addComment();
 renderPeople();
