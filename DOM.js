e13bceaf0c886307f4bc68033a70253471115785
KateLamp <evka2709@gmail.com>
2023-08-19 17:12:31 +0300

    Доработка даты и времени

--- DOM.js
+++ DOM.js
@@ -17,13 +17,19 @@ buttonElement.addEventListener("click", () => {
         return;
     };
 
-
+    function formatDate(date) {
+      const locale = 'ru-RU'
+      const options = {year: '2-digit', month: 'numeric', day: 'numeric'};
+      return `${date.toLocaleString(locale, options)} ${date.toLocaleTimeString(locale, {hour: '2-digit', minute: '2-digit'})}`
+   };
+   
+    /*
     let myDate = new Date(); 
-    let day = myDate.getDay();
-    let month = myDate.getMonth();
-    let year = myDate.getFullYear();
-    let hour = myDate.getHours(); 
-    let minute = myDate.getMinutes(); 
+    let day = String(myDate.getDay()).padStart(2, '0');
+    let month = String(myDate.getMonth()).padStart(2, '0');
+    let year = String(myDate.getFullYear()).padStart(2, '0');
+    let hour = String(myDate.getHours()).padStart(2, '0'); 
+    let minute = String(myDate.getMinutes()).padStart(2, '0'); 
     if (day < 10) { 
         day = "0" + day; 
     };
@@ -37,13 +43,14 @@ buttonElement.addEventListener("click", () => {
         minute = "0" + minute; 
     };
     let time = day + "." + month + "." + year + " " + hour + ":" + minute;
+    */
 
     const oldListHtml = listElement.innerHTML;
     listElement.innerHTML = oldListHtml + 
     `<li class="comment">
     <div class="comment-header">
       <div>${nameInputElement.value}</div>
-      <div> ${time} </div>
+      <div> ${formatDate(new Date())} </div>
     </div>
     <div class="comment-body">
       <div class="comment-text">
