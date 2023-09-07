112292379e32806b10cb68c6ec2ce3116bceffa4
YanaOrlova93 <orlova93zm@mail.ru>
2023-09-07 22:21:49 +0300

    hw9

---
+++ Новая папка/script.js
@@ -0,0 +1,32 @@
+
+
+
+// let i = 0;
+// while (i < 6) {
+//   console.log(i);
+//   i++;
+// }
+
+// while (true) {
+//   console.log('привет');
+// }
+
+// let ii = 0;
+// do {
+//   console.log(ii);;
+//   i++;
+// } while (i <6);
+
+
+// let i = 1;
+
+// let n = Number (prompt('Сколько раз'));
+//  while (i <= n) {
+//   alert('привет');
+//   i++;
+//  }
+
+let number;
+do {
+  number = Number(prompt('Введите 5 для проверки '));
+} while (number !== 5 );
