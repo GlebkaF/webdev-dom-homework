9ea06e3216cbab044c9b6e5367c433082ece7a5f
maksrover <maksrover@gmail.com>
2023-09-24 14:16:39 +0300

    main

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const addCommentButton = document.getElementById("add-comment-button");
 const deleteCommentButton = document.getElementById("deleteCommentButton");
 
 addCommentButton.addEventListener("click", () => {
+  
   addComment();
 });
 
 const commentsList = document.getElementById("comments-list");
 commentsList.addEventListener("click", (event) => {
-  const target = event.target;  
+  const target = event.target;
   if (target.classList.contains("like-button")) {
     handleLikeButtonClick(target);
   }
