3d7208b327a3698efd089f0f14e3dbe0b4040ddf
maksrover <maksrover@gmail.com>
2023-09-26 11:03:32 +0300

    data.js deleted

--- data.js
+++
@@ -1,4 +0,0 @@
-export function date () { 
-    const currentDate = new Date();
-    const formattedDate = `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}`;
-  };
