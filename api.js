5a2cf775f25c7457b63681618c559e06c4198108
XshadowE <nimro-dell@mail.ru>
2023-10-10 14:07:22 +0700

    added 8

--- api.js
+++ api.js
@@ -1,9 +1,15 @@
 const formsInputElement = document.querySelector(".add-form");
 const commentEditText = document.querySelectorAll("comment-edit-text");
 
+let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+const host = "https://wedev-api.sky.pro/api/v2/evgeniya-ko/comments";
+
 export function getComments() {
-  return fetch("https://wedev-api.sky.pro/api/v1/evgeniya-ko/comments", {
+  return fetch(host, {
     method: "GET",
+    headers: {
+      Authorization: token,
+    },
   }).then((response) => {
     return response.json();
   });
@@ -13,13 +19,16 @@ export function postComment({ name, text }) {
   if (name < 3 || text < 3) {
     throw new Error("Ошибка");
   }
-  return fetch("https://wedev-api.sky.pro/api/v1/evgeniya-ko/comments", {
+  return fetch(host, {
     method: "POST",
     body: JSON.stringify({
       name: name,
       text: text,
       forceError: false,
     }),
+    headers: {
+      Authorization: token,
+    },
   })
     .then((response) => {
       console.log(response);
