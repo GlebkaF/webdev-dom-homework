3983662908c6e2a1e909be85fbe7502574d993bc
XshadowE <nimro-dell@mail.ru>
2023-10-10 22:43:04 +0700

    fix1

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 "use strict";
 
 import { getComments } from "./api.js";
-import { postComment } from "./api.js";
+
 import { renderLogin } from "./loginPage.js";
 import { renderComments } from "./renderComments.js";
 
-const buttonElement = document.getElementById("add-button");
-const nameInputElement = document.getElementById("name-input");
-const textInputElement = document.querySelector(".add-form-text");
-const textareaInputElement = document.getElementById("textarea-input");
+
 
 
 // Массив данных из хранилища
@@ -30,81 +27,3 @@ const fetchAndRenderComments = () => {
 };
 
 renderLogin({fetchAndRenderComments});
-//renderComments(comments);
-
-/* код для проверки, что комментарий не пустой и отправки комментария 
-    скопировали код nameInput чтобы не писать его дважды, тк одно действие и те же проверки
-    */
-const onSubmit = () => {
-  nameInputElement.classList.remove("error");
-  if (nameInputElement.value === "") {
-    nameInputElement.classList.add("error");
-    return;
-  }
-  textareaInputElement.classList.remove("error");
-  if (textareaInputElement.value === "") {
-    textareaInputElement.classList.add("error");
-    return;
-  }
-
-  // Кнопка написать не активна при пустых значениях //
-  buttonElement.setAttribute("disabled", true);
-  nameInputElement.oninput = function () {
-    if (nameInputElement.value === "" && textareaInputElement.value === "") {
-      buttonElement.setAttribute("disabled", true);
-      return;
-    } else {
-      buttonElement.removeAttribute("disabled");
-      return;
-    }
-  };
-
-  // Добавляем изменение кнопки на Элемент добавляется
-  const startAt = Date.now();
-
-  buttonElement.disabled = true;
-  buttonElement.textContent = "Элемент добавляется...";
-
-  // POST
-  postComment({
-    name: nameInputElement.value,
-    text: textInputElement.value,
-  })
-    .then((response) => {
-      console.log("Время: " + (Date.now() - startAt));
-      return response;
-    })
-    .then(() => {
-      fetchAndRenderComments();
-    })
-    .then(() => {
-      buttonElement.disabled = false;
-      buttonElement.textContent = "Написать";
-      nameInputElement.value = "";
-      textareaInputElement.value = "";
-    })
-    // Интернет отключили
-    .catch((response) => {
-      buttonElement.disabled = false;
-      buttonElement.textContent = "Написать";
-      if (response.status === 400) {
-        alert("Пропал интернет, попробуй позже");
-      } else {
-        return response;
-      }
-    });
-
-  renderComments(comments);
-};
-
-// Добавили отправку коммента по нажатию на Enter
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    onSubmit();
-  }
-});
-
-renderComments(comments);
-
-// Вынесли код обработчика в отдельную функцию onSubmit - (enterPress)
-buttonElement.addEventListener("click", onSubmit);
--- renderComments.js
+++ renderComments.js
@@ -1,5 +1,6 @@
 import { initDeliteButtonsListeners, initEventlikes } from "./Initialization.js";
 import { answerText } from "./answer.js";
+import { postComment } from "./api.js";
 
 const commentsElement = document.getElementById("comments");
 
@@ -54,8 +55,86 @@ export const renderComments = (comments) => {
       </div>`
   
     appElement.innerHTML = appHtml;
+
+    const buttonElement = document.getElementById("add-button");
+const nameInputElement = document.getElementById("name-input");
+const textInputElement = document.querySelector(".add-form-text");
+const textareaInputElement = document.getElementById("textarea-input");
     
-      
+    const onSubmit = () => {
+      nameInputElement.classList.remove("error");
+      if (nameInputElement.value === "") {
+        nameInputElement.classList.add("error");
+        return;
+      }
+      textareaInputElement.classList.remove("error");
+      if (textareaInputElement.value === "") {
+        textareaInputElement.classList.add("error");
+        return;
+      }
+    
+      // Кнопка написать не активна при пустых значениях //
+      buttonElement.setAttribute("disabled", true);
+      nameInputElement.oninput = function () {
+        if (nameInputElement.value === "" && textareaInputElement.value === "") {
+          buttonElement.setAttribute("disabled", true);
+          return;
+        } else {
+          buttonElement.removeAttribute("disabled");
+          return;
+        }
+      };
+    
+      // Добавляем изменение кнопки на Элемент добавляется
+      const startAt = Date.now();
+    
+      buttonElement.disabled = true;
+      buttonElement.textContent = "Элемент добавляется...";
+    
+      // POST
+      postComment({
+        name: nameInputElement.value,
+        text: textInputElement.value,
+      })
+        .then((response) => {
+          console.log("Время: " + (Date.now() - startAt));
+          return response;
+        })
+        .then(() => {
+          fetchAndRenderComments();
+        })
+        .then(() => {
+          buttonElement.disabled = false;
+          buttonElement.textContent = "Написать";
+          nameInputElement.value = "";
+          textareaInputElement.value = "";
+        })
+        // Интернет отключили
+        .catch((response) => {
+          buttonElement.disabled = false;
+          buttonElement.textContent = "Написать";
+          if (response.status === 400) {
+            alert("Пропал интернет, попробуй позже");
+          } else {
+            return response;
+          }
+        });
+    
+      renderComments(comments);
+    };
+    
+    // Добавили отправку коммента по нажатию на Enter
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        onSubmit();
+      }
+    });
+    
+    renderComments(comments);
+    
+    // Вынесли код обработчика в отдельную функцию onSubmit - (enterPress)
+    buttonElement.addEventListener("click", onSubmit);
+    
 
     answerText();
     initEventlikes();
