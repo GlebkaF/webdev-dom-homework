b591606e216b586813b7589d09b94b9dfe89ba3a
Ulyana Lazutina <ulyalazutina@yandex.ru>
2023-10-14 13:47:37 +0300

    Выполнила обязательную часть домашнего задания

--- hw-9.js
+++ hw-9.js
@@ -1,93 +1,212 @@
-"use strict";
-//Поиск всех элементов
-const addButtonElement = document.querySelector('.add-form-button');
-const commentsElement = document.querySelector('.comments');
-const nameInputValue = document.querySelector('.add-form-name');
-const textareaValue = document.querySelector('.add-form-text');
-const formElement = document.querySelector('.add-form');
-let dateValue = document.querySelector('.time');
-const removeButtonElement = document.querySelector('.remove-button')
-const commentElement = document.querySelectorAll('.comment');
-//Работа с кнопкой
-addButtonElement.disabled = true;
-addButtonElement.classList.add('btnNoActive');
+// "use strict";
+//Нашла два инпута
+const nameInputElement = document.querySelector('.add-form-name');
+const commentInputElement = document.querySelector('.add-form-text');
 
-//Колбэк функция для активации кнопки
-function isActive() {
-    if (nameInputValue.value !== '' && textareaValue.value !== '') {
-        addButtonElement.disabled = false;
-        addButtonElement.classList.remove('btnNoActive');
-    }
+//Нашла блок с комментариями и сами комментарии
+const blockComments = document.querySelector('.comments');
+
+
+//Нашла кнопку написать и отключила ее
+const writeButton = document.querySelector('.add-form-button');
+writeButton.disabled = true;
+writeButton.style.backgroundColor = 'grey';
+
+//Нашла форму добавления комментариев
+const formAddComm = document.querySelector('.add-form');
+
+//Нашла кнопку удаления
+const deleteButton = document.querySelector('.remove-button');
+
+
+
+//массив с комментариями
+const comments = [
+  {
+    name: 'Глеб Фокин',
+    textComment: 'Это будет первый комментарий на этой странице',
+    isEdit: false,
+    date: '12.02.22 12:18',
+    isLike: true,
+    countLike: 3,
+  },
+  {
+    name: 'Варварва Н.',
+    textComment: 'Мне нравится как оформлена эта страница! ❤',
+    isEdit: false,
+    date: '13.02.22 19:22',
+    isLike: false,
+    countLike: 75,
+  },
+];
+//Отслеживает лайки
+const initLikeButtonsListener = () => {
+  //Нашла кнопку лайка
+  const likeButtons = document.querySelectorAll('.like-button');
+  //Перебираю все кнопки 
+  for (const likeButton of likeButtons) {
+    likeButton.addEventListener('click', () => {
+      //Присвоила индексу весь элемент массива
+      const index = likeButton.dataset.index;
+      if (comments[index].isLike) {
+        comments[index].isLike = false;
+        comments[index].countLike -= 1;
+      } else {
+        comments[index].isLike = true;
+        comments[index].countLike += 1;
+      }
+      renderComments();
+    });
+  }
 }
+
+
 //Отслеживает инпуты
-nameInputValue.addEventListener('input', isActive);
-textareaValue.addEventListener('input', isActive);
-
-//Колбэк функция для добавления комментариев
-function addComments() {
-    //Сохранение предыдущих комментариев
-    const oldItemElement = commentsElement.innerHTML;
-    //Создание даты
-    dateValue = new Date();
-    const month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-    let day = dateValue.getDate();
-    if (day < 10) {
-        day = '0' + day;
-    }
-    let year = dateValue.getFullYear().toString().slice(-2);
-    //Удаление классов у текстовых полей
-    nameInputValue.classList.remove('error');
-    textareaValue.classList.remove('error');
-    //Валидация полей
-    if (nameInputValue.value === '' && textareaValue.value === '') {
-        nameInputValue.classList.add('error');
-        textareaValue.classList.add('error');
-        return;
-    } else if (nameInputValue.value === '') {
-        nameInputValue.classList.add('error');
-        return;
-    } else if (textareaValue.value === '') {
-        textareaValue.classList.add('error');
-        return;
-    }
-    //Добавление нового комментария
-    commentsElement.innerHTML = oldItemElement + `<li class="comment">
-    <div class="comment-header">
-      <div>${nameInputValue.value}</div>
-      <div class="time">${day}.${month[dateValue.getMonth()]}.${year} ${dateValue.getHours()}:${dateValue.getMinutes()}</div>
-    </div>
-    <div class="comment-body">
-      <div class="comment-text">
-        ${textareaValue.value}
-      </div>
-    </div>
-    <div class="comment-footer">
-      <div class="likes">
-        <span class="likes-counter">0</span>
-        <button class="like-button"></button>
-      </div>
-    </div>
-  </li>`
-    //Очищение полей после добавления комментария
-    textareaValue.value = '';
-    nameInputValue.value = '';
-    addButtonElement.disabled = true;
-    addButtonElement.classList.add('btnNoActive');
+const isActive = () => {
+  if (nameInputElement.value !== '' && commentInputElement.value !== '') {
+    writeButton.disabled = false;
+    writeButton.style.backgroundColor = '#bcec30';
+  }
 }
-//Добавление комментарий по клику
-addButtonElement.addEventListener('click', addComments)
-//Добавление комментария по нажатию Энтер
-formElement.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        addComments();
-    }
+
+nameInputElement.addEventListener('input', isActive);
+commentInputElement.addEventListener('input', isActive);
+
+// const initSaveButtonsListener = () => {
+//   const saveButton = document.querySelector('.save-btn');
+//   const updateElements = document.querySelector('.update-form-text');
+//   saveButton.addEventListener('click', () => {
+//     const index = saveButton.dataset.index;
+//     comments[index].textComment = updateElements.value;
+//     console.log(comments[index]);
+
+//   })
+//   console.log(saveButton);
+// }
+
+// const initUpdateButtonsListener = () => {
+//   const commentElements = document.querySelectorAll('.comment');
+//   const updateButtons = document.querySelectorAll('.update-btn');
+//   for (const updateButton of updateButtons) {
+//     const index = updateButton.dataset.index;
+//     updateButton.addEventListener('click', () => {
+//       commentElements[index].innerHTML = `
+//       <div class="comment-header">
+//           <div></div>
+//           <div>13.02.22 19:22</div>
+//         </div>
+//         <div class="comment-body">
+//         <textarea
+//         type="textarea"
+//         class="update-form-text"
+//         placeholder="Введите ваш коментарий"
+//         rows="4"
+//       >
+//       Мне нравится как оформлена эта страница! ❤
+//       </textarea>   
+//         </div>
+//         <div class="comment-footer">
+//           <div class="likes">
+//             <span class="likes-counter">75</span>
+//             <button data-index="1" class="like-button"></button>
+//             <button data-index="1" type="button" class="save-btn">Сохранить</button>
+//             </div>
+//         </div>
+//       `
+//       console.log(commentElements[index].innerHTML);
+//       save();
+//     })
+
+//   }
+// }
+
+//Рендерит комментарии
+const renderComments = () => {
+  blockComments.innerHTML = comments.map((comment, index) => {
+    return `
+        <li class="comment">
+        <div class="comment-header">
+          <div>${comment.name}</div>
+          <div>${comment.date}</div>
+        </div>
+        <div class="comment-body">
+          <div class="comment-text">
+          ${comment.textComment}
+          </div>
+        </div>
+        <div class="comment-footer">
+          <div class="likes">
+            <span class="likes-counter">${comment.countLike}</span>
+            <button data-index='${index}' class="${comment.isLike ? 'like-button -active-like' : 'like-button'}"></button>
+            <button data-index='${index}' type="button" class="update-btn">Редактировать</button>
+            </div>
+        </div>
+      </li>
+        `
+  }).join('');
+
+
+  initLikeButtonsListener();
+  // initUpdateButtonsListener();
+}
+renderComments();
+
+
+
+
+
+//Функция добавления комментария
+const addComments = () => {
+  //Работа со временем
+  const datePublish = new Date(); //создание времени
+  const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']; //Правильная расстановка месяцей
+  let dayPublish = datePublish.getDate(); //Получение дня
+  let monthPublish = months[datePublish.getMonth()]; //Получение месяца
+  let yearPublish = datePublish.getFullYear().toString().slice(-2); //Получение года
+  let hoursPublish = datePublish.getHours(); //Получение часов
+  let minutesPublish = datePublish.getMinutes(); //Получение минус
+  //Проверки на нули для красоты
+  if (dayPublish < 10) {
+    dayPublish = '0' + dayPublish;
+  }
+  if (hoursPublish < 10) {
+    hoursPublish = '0' + hoursPublish;
+  }
+  if (minutesPublish < 10) {
+    minutesPublish = '0' + minutesPublish;
+  }
+  //Передача всех параметров в одну переменную
+  const newFormatDatePublish = `${dayPublish}.${monthPublish}.${yearPublish} ${hoursPublish}:${minutesPublish}`;
+
+  //Добавление в массив новые комменатрии
+  comments.push({
+    name: nameInputElement.value,
+    textComment: commentInputElement.value,
+    date: newFormatDatePublish,
+    isLike: false,
+    countLike: 0,
+  });
+
+  renderComments();
+  nameInputElement.value = '';
+  commentInputElement.value = '';
+  writeButton.disabled = true;
+  writeButton.style.backgroundColor = 'grey';
+}
+
+//При нажатии на кнопку "Написать" добавляется новый комментрий
+writeButton.addEventListener('click', addComments)
+
+//При нажатии на энтер добавляется новый комментарий
+formAddComm.addEventListener('keyup', (elem) => {
+  if (elem.keyCode === 13) {
+    addComments();
+  }
 })
 
-//Удаление
-removeButtonElement.addEventListener('click', () => {
-    let lastIndex = commentsElement.innerHTML.lastIndexOf(`<li class="comment">`);
-    console.dir(lastIndex)
-    // commentsElement.innerHTML = 
-    // let a = lastIndex.slice(0, lastIndex);
-    // lastEl.remove();
+// //Удаление последнего комментария
+deleteButton.addEventListener('click', () => {
+  const lastCom = comments.at(-1);
+  comments.splice(lastCom, 1);
+  renderComments();
 })
