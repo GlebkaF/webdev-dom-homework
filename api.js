3da1fde43d8d7c4217c9b6153dc617d528513aa7
Elena Bersh <lenaber6@mail.ru>
2023-12-23 19:58:55 +0500

    modul_js with Qs

---
+++ api.js
@@ -0,0 +1,38 @@
+export function getTodos() {
+    return fetch("https://wedev-api.sky.pro/api/v1/elena-bersh/comments", {
+        method: "GET",
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else if (response.status === 500) {
+                alert("Сервер сломался, попробуйте позже");
+                throw new Error("Сервер сломался");
+            }
+        });  
+}
+export function postTodo({text, name}) {
+return fetch("https://wedev-api.sky.pro/api/v1/elena-bersh/comments", {
+    method: "POST",
+    body: JSON.stringify({
+        text: text,
+        name: name,
+        // Проверка кода на 500 ошибку. POST-запрос будет в половине случаев отвечать 500-й ошибкой,
+         // если в теле запроса передать параметр forceError: true
+        forceError: true,
+    }),
+})
+    .then((response) => {
+        if (response.status === 201) {
+            return response.json();
+        } else if (response.status === 500) {
+            alert("Сервер сломался, попробуйте позже");
+            throw new Error("Сервер сломался");
+        } else {
+            // alert ставить перед throw, если поменять местами, то алёрт не показывает.
+            alert("Вы ввели меньше трёх символов в поле ввода имени или комментария. Введите, пожалуйста, заново.");
+            throw new Error("Имя или комментарий короче 3х символов");
+        }
+
+    });
+}
---
+++ renderStudentsComments.js
@@ -0,0 +1,31 @@
+// Рендер-функция отрисовывает новые комменты
+const listElement = document.getElementById('list');
+export const renderStudentsComments = ({studentsComments, fetchAndRenderComments}) => {
+
+    const studentsHtml = studentsComments
+    .map((comment, index) => {
+        return `<li class="comment" data-index="${index}">
+<div class="comment-header">
+<div>${comment.name}</div>
+<div>${comment.date}</div>
+</div>
+<div class="comment-body">
+<div class="comment-text">
+  ${comment.text}
+</div>
+</div>
+<div class="comment-footer">
+<div class="likes">
+  <span class="likes-counter">${comment.likes}</span>
+  <button class="like-button ${studentsComments[index].isLiked ? "-active-like" : ""}" data-index="${index}"></button>
+</div>
+</div>
+</li>`;
+    })
+        .join("");
+    // console.log(studentsHtml);
+    listElement.innerHTML = studentsHtml;
+    initLikeListeners();
+    initAnswerComments();
+
+}
