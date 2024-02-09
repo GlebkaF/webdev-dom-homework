c8d9bc24b8d521e340b368e5def78d737029c119
Dmitry Avdoshkin <works-dim@yandex.ru>
2024-02-10 00:40:14 +0800

    Выполнение_DZ-02.09_Dom-1_v3

--- HW-09.js
+++ HW-09.js
@@ -3,70 +3,69 @@
 const name_Input_Element = document.querySelector(".add-form-name");
 const comment_Input_Element = document.querySelector(".add-form-text");
 const button_Element = document.querySelector(".add-form-button");
+const comments_Element = document.querySelector(".comments");
+let currentDate = new Date();
+let year = currentDate.getFullYear().toString().slice(2);
+let month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+let day = ('0' + currentDate.getDate()).slice(-2);
+let hours = ('0' + currentDate.getHours()).slice(-2);
+let minutes = ('0' + currentDate.getMinutes()).slice(-2);
+let formattedDateTime = day + '.' + month + '.' + year + ' ' + hours + ':' + minutes;
 
-button_Element.addEventListener("click", add_New_Comment);
-function add_New_Comment() {
-  let commentList = document.querySelector(".comments")
-  commentList.innerHTML += `
-    <li class="comment">
-      <div class="comment-header">
-        <div>${name_Input_Element.value}</div>
-        <div>${(new Date()).toLocaleString()}</div>
-      </div>
-      <div class="comment-body">
-        <div class="comment-text">
-          ${comment_Input_Element.value}
-        </div>
-      </div>
-      <div class="comment-footer">
-        <div class="likes">
-          <span class="likes-counter">0</span>
-          <button class="like-button"></button>
-        </div>
-      </div>
-    </li>`;
+// Установка формата даты ДД.ММ.ГГГГ ЧЧ:ММ
+const date = function () {
+  const months = ["01", "02", "03", "04", "05", "06",
+  "07", "08", "09", "10", "11", "12"];
+  let commentDate = new Date();
+    let currentDate = commentDate.getDate() + "." + months[commentDate.getMonth()] + "." + commentDate.getFullYear();
+    let hour = commentDate.getHours();
+    let minute = commentDate.getMinutes();
+    let second = commentDate.getSeconds();
+      if (minute < 10) {
+        minute = "0" + minute;
+      }
+      let currentTime = hour + ":" + minute;
+      let fullDate = `${currentDate} ${currentTime}`;
+      return fullDate
+}
 
-  function input() {
-    if (name_Input_Element.value == "" || comment_Input_Element.value == ""){
-      button_Element.setAttribute("disabled", true);
-    } else {
-      button_Element.removeAttribute("disabled");
-    }
+// Функция клика, валидация
+button_Element.addEventListener("click", () => {
+  name_Input_Element.classList.remove("error");
+  comment_Input_Element.classList.remove("error");
+  button_Element.classList.remove("disabled-button");
+  if (name_Input_Element.value === "" || comment_Input_Element.value === "") {
+    name_Input_Element.classList.add("error");
+    comment_Input_Element.classList.add("error");
+    button_Element.classList.add("disabled-button");
+    return;
   }
+// Добавляем новый комментарий
+  const oldListElement = comments_Element.innerHTML;
+  comments_Element.innerHTML = oldListElement + `<li class="comment">
+    <div class="comment-header">
+      <div>${name_Input_Element.value}</div>
+      <div>${formattedDateTime}</div>
+    </div>
+    <div class="comment-body">
+      <div class="comment-text">${comment_Input_Element.value}</div>
+    </div>
+    <div class="comment-footer">
+      <div class="likes">
+        <span class="likes-counter">0</span>
+        <button class="like-button"></button>
+      </div>
+    </div>
+  </li>`;
 
-  name_Input_Element.addEventListener("input", input);
-  comment_Input_Element.addEventListener("input", input);
+name_Input_Element.value = "";
+comment_Input_Element.value = "";
+});
 
-  button_Element.addEventListener('click', () => {
-    name_Input_Element.classList.remove("error");
-    comment_Input_Element.classList.remove("error");
-    if (name_Input_Element.value === "" && comment_Input_Element.value === "") {
-        name_Input_Element.classList.add("error");
-        comment_Input_Element.classList.add("error");
-        return;
-
-    } else if (name_Input_Element.value === ""){
-        name_Input_Element.classList.add("error")
-        return;
-
-    } else if (comment_Input_Element.value === ""){
-        comment_Input_Element.classList.add("error")
-        return;
-    }
-        const old_List_Element = commentList.innerHTML;
-        commentList.innerHTML = old_List_Element +  `
-        <li class="comment">
-            <div class="comment-header">
-                <div>${name_Input_Element.value}
-                    @@ -127,7 +123,7 @@
-                </div>
-            </div>
-        </li>`;
-        name_Input_Element.value = "";
-        textInputElement.value = "";
-  });
-  comment_Input_Element.value = "";
-  //console.log(commentList)
-  //console.log(name_Input_Element.value)
-  //console.log(comment_Input_Element.value)
-};
+// Добавление комментрия клавишей Enter
+document.addEventListener("keyup", (event) => {
+  if (event.code === 'Enter') {
+    document.querySelector(".add-form-button").click();
+    return;
+  }
+});
