18250f261ca3b8c098e77307ab50b5efa3be10ab
Fnami51 <fnm5160@gmail.com>
2024-02-12 14:23:59 +0200

    Правка домашнего задания

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-"use strict";
-
 const buttonElement = document.getElementById('comment-button');
 const nameElement = document.getElementById('comment-author');
 const textElement = document.getElementById('comment-text');
 const listComments = document.getElementById("comments");
 
+function twoDigits(num) {
+  if( num >= 0 && num <= 9) {
+    return "0" + num;
+  } else { 
+    return "" + num;
+  }
+}
+
 buttonElement.addEventListener("click", () => {
     nameElement.classList.remove("error");
     textElement.classList.remove("error");
     buttonElement.classList.remove("error-for-button");
-    console.log('Button is pressed')
-    if (nameElement.value === "" && textElement.value === "") {
+    if (nameElement.value === "" || textElement.value === "") {
         nameElement.classList.add("error");
         textElement.classList.add("error");
         buttonElement.classList.add("error-for-button");
         return;
-    };
+    }; 
     let commentDate = new Date();
     const oldComments = listComments.innerHTML;
     listComments.innerHTML =
@@ -23,7 +28,7 @@ buttonElement.addEventListener("click", () => {
         `<li class="comment">
         <div class="comment-header">
           <div>${nameElement.value}</div>
-          <div>${commentDate.getDay()}.${commentDate.getMonth()}.${commentDate.getFullYear()} ${commentDate.getHours()}:${commentDate.getMinutes()}</div>
+          <div>${twoDigits(commentDate.getDate())}.${twoDigits(commentDate.getMonth()+1)}.${commentDate.getFullYear()} ${twoDigits(commentDate.getHours())}:${twoDigits(commentDate.getMinutes())}</div>
         </div>
         <div class="comment-body">
           <div class="comment-text">
@@ -38,5 +43,3 @@ buttonElement.addEventListener("click", () => {
         </div>
       </li>`;
 });
-
-console.log("It works!");
