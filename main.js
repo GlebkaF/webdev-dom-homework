b937d3875a81ee751e8dca6d99974447552879b2
fedotovps <fedotovps@bk.ru>
2024-02-18 13:23:49 +0300

    2.16: fix

--- main.js
+++ main.js
@@ -148,49 +148,16 @@ import { renderCom } from "./renderComments.js";
   // Функция рендера комментариев
 
   const renderComments = () => {
-
-    const CommentsHtml = comments.map((comment, index) => {
-        let isLike;
-        let inputTextHtml;
-        let textButtonEditSave;
-        let classButtonEditSave;
-        //isLoadedPage = true;
-        comment.myLike ? isLike = "-active-like" : false
-        
-        comment.isEdit ? textButtonEditSave = "Сохранить" : textButtonEditSave = "Редактировать"
-        comment.isEdit ? classButtonEditSave = "comment-text-save" : classButtonEditSave = "comment-text-edit"
-        comment.isEdit ? inputTextHtml = `<textarea id="form-text" type="textarea" class="add-form-text" placeholder="Введите ваш коментарй" rows="4">${comment.text}</textarea>` : inputTextHtml = `<div data-index="${index}" class="comment-text">${comment.text}</div>`;
-    
-        
-          return `<li class="comment">
-                  <div class="comment-header">
-                    <div>${comment.author}</div>
-                    <div>${comment.date}</div>
-                  </div>
-                  <div class="comment-body">
-                    ${inputTextHtml}
-                  </div>
-                  <button data-edit="${index}" class="${classButtonEditSave}">${textButtonEditSave}</button>
-                  <div class="comment-footer">
-                    <div class="likes">
-                      <span class="likes-counter">${comment.likeCount}</span>
-                      <button class="like-button ${isLike}" id="like-button" data-islike="${index}"></button>
-                    </div>
-                  </div>
-                </li>`;
-        
-        
-                
-      }).join("");
     
     commentList.innerHTML = renderCom(comments);
     ititAddLikeListener();
     initEditCommentListener();
     initSaveEditCommentListener();
+    //initAnswerComment();
     initAnswerComment2();
   };
 
-  //renderComments();
+  renderComments();
   renderLogin();
 
   const postTask = () => {
