44c4b9d0b86890d8bbfab9aaf23ebb01c1defc2d
здесь ваше имя <  >
2024-03-19 20:37:25 +0700

    14

--- main.js
+++ main.js
@@ -48,23 +48,23 @@ buttonElement.addEventListener("click", () => {
     buttonElement.textContent = 'Добавление...';
     const startAt = Date.now();
     console.log("Начинаем делать запрос");
-   
-        fetch("https://wedev-api.sky.pro/api/v1/alenka-s/comments", {
-             method: "POST",
-               body: JSON.stringify({
-                   name: nameInputElement.value
-                       .replaceAll("<", "&lt")
-                       .replaceAll(">", "&gt")
-                       .replaceAll("&", "&amp;")
-                       .replaceAll('"', "&quot;"),
-                   text: commentInputElement.value
-                       .replaceAll("<", "&lt")
-                       .replaceAll(">", "&gt")
-                       .replaceAll("&", "&amp;")
-                       .replaceAll('"', "&quot;"),
-                   forceError: true,
-               }),
-           }) 
+
+    fetch("https://wedev-api.sky.pro/api/v1/alenka-s/comments", {
+        method: "POST",
+        body: JSON.stringify({
+            name: nameInputElement.value
+                .replaceAll("<", "&lt")
+                .replaceAll(">", "&gt")
+                .replaceAll("&", "&amp;")
+                .replaceAll('"', "&quot;"),
+            text: commentInputElement.value
+                .replaceAll("<", "&lt")
+                .replaceAll(">", "&gt")
+                .replaceAll("&", "&amp;")
+                .replaceAll('"', "&quot;"),
+            forceError: true,
+        }),
+    })
         //Ошибки       
         .then((response) => {
             console.log(response);
